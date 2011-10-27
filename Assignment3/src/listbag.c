873d891d61c91a74a11fb48a2679d5610163c71d
Geoffrey Corey <coreyg@engr.orst.edu>
2011-10-27 00:25:45 -0700

    re-added assignemnt 3 skeleton code

---
+++ Assignment3/src/listbag.c
@@ -0,0 +1,45 @@
+#include "listbag.h"
+#include <assert.h>
+#include "type.h"
+#include <stdlib.h>
+
+/*Bag interface*/
+/* Function to initialize bag:
+	Pre: b is not null
+	Post: list implementing the bag is initialized, size = 0
+*/
+void initBag(struct bag* b){
+	assert( b != NULL);
+	b->lst = (struct list*)malloc(sizeof(struct list));/*Allocate for the list implementing the bag*/
+	initList(b->lst);/*Initialize list*/
+}
+
+/* Function to add an element to the bag:
+	Pre: b is not null
+	Post: val is added to the bag ie the list implementing the bag*/
+void addToBag(struct bag* b, TYPE val){
+ 	/*FIX ME*/
+}
+
+/* Function to remove an element from the bag:
+	Pre: b is not null
+	Post: Element (if found) removed from bag/list
+*/
+void removeFromBag(struct bag* b, TYPE val){
+	/*FIX ME*/
+}
+
+/*Function to test if an element exists in the bag
+	Pre: b is not null
+*/
+int bagContains(struct bag* b, TYPE val){
+	/* FIX ME*/
+}
+
+/*Function to test if the bag is empty
+	Pre: b is not null
+*/
+
+int isEmptyBag(struct bag* b){
+	/* FIX ME*/
+}
