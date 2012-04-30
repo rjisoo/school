119bdacdc8b3ed57d05bd661d373db161d7f43a1
Geoffrey Corey <coreyg@onid.orst.edu>
2012-04-30 15:43:37 -0700

    added small test function

--- usart-interrupt/main.c
+++ usart-interrupt/main.c
@@ -1,9 +1,18 @@
+#include <avr/io.h>
+#include <stdlib.h>
 #include "usart.h"
 #include "wunderboard.h"
 
 
 
 int main (void){
+
+	initWunderboard();
+	initializeUSART();
+
+	char string[] = "this is a test\r\n";
+
+	SendStringUSART((uint8_t *) string);
 
 	return 0;
 }
