60c1d74e6e18d580bda7806508a79ced5cd17f04
Geoffrey Corey <coreyg@onid.orst.edu>
2012-05-02 18:17:02 -0700

    flder cleanup and branch cleanup

--- interrupt/adc.h
+++
@@ -1,6 +0,0 @@
-#ifndef ADC_H
-#define ADC_H
-
-unsigned char read_adc(uint8_t channel);
-
-#endif
--- interrupt/sdcard.h
+++
@@ -1,8 +0,0 @@
-#ifndef SDCARD_H
-#define SDCARD_H
-
-void printErrorUART(uint8_t err);
-
-uint8_t initializeFAT(FATFS* fs);
-
-#endif
