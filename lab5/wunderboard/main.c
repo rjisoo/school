a348db85f3f0fffb5f19955228e151dfffada34a
Geoffrey Corey <coreyg@onid.orst.edu>
2012-05-09 14:21:17 -0700

    safety commit

--- lab5/wunderboard/main.c
+++ lab5/wunderboard/main.c
@@ -13,57 +13,14 @@
 #include <stdio.h>
 #include "usart.h"
 #include "wunderboard.h"
-#include "timer.h"
-#include "adc.h"
 
-FILE uart_io = FDEV_SETUP_STREAM(SendByteUSART1, NULL, _FDEV_SETUP_WRITE);
 volatile enum states {init, idle, start, shutdown, stop} state;
 volatile uint8_t count;
 
 int main (void){
-	/* Directs serial communication to stdout for usual print and functions */
-	stdout = &uart_io;
-	state = init;
 
-	while (1){
-		switch (state){
 
-		case init:
-			/*
-			 * Setup wunderboard, initialize USART, start global interrupts.
-			 * Also, print directions.
-			 */
-			initWunderboard();
-			clearArray();
-			initializeUSART1();
-			initializeTIMER0();
-			visualUpDown(1);
-			count = 0;
-			sei();
-			printf("Press 's' to start.\n");
-			state = idle;
-			break;
 
-		case idle:
-			/*
-			 * Wait for input from USART to match 's'.
-			 */
-			break;
-
-		case start:
-			break;
-
-		case shutdown:
-			printf("Shutting down...\n");
-			_delay_ms(50);
-			state = stop;
-			break;
-
-		case stop:
-			visualUpDown(0);
-			break;
-		}
-	}
 	return 0;
 }
 
@@ -78,17 +35,6 @@ ISR (USART1_RX_vect){
 			stopTIMER0();
 			state = shutdown;
 		}
-	}
-}
-
-ISR (TIMER0_COMPA_vect) {
-	count++;
-	if (count > 1) {
-		printf("ADC Value: %d.\n", read_adc(PF5));
-		setArrayAmber(0b00111100);
-		count = 0;
-	} else {
-		clearArray();
 	}
 }
 
