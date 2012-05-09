9781db359485b3b330b6386855428188db6f0542
Geoffrey Corey <coreyg@onid.orst.edu>
2012-05-09 09:42:01 -0700

    going to move to virtualized ubuntu

--- lab5/terminal/lab5.cpp
+++ lab5/terminal/lab5.cpp
@@ -12,19 +12,19 @@ int main(int argc, char* argv[])
 	string inputstr;
 	cout << "Lab 5 Code Started" << endl;
 
-	/*When you are dealing with IO devices, it is vaery important that calls to them be wrapped
+	/*When you are dealing with IO devices, it is very important that calls to them be wrapped
 	 * in 'try-catch' blocks. These blocks of code tell the program 'if something goes
 	 * wrong (an exception) run whatever is in catch rather than simply killing the program.
 	*/
     try { 
 
 		// Make a SimpleSerial object with the parameter for your Wunderboard/OS
-
+    	SimpleSerial comm("/dev/ttyUSB0", 9600);
 		// Start reading data from your wunderboard and displaying it to the screen.
 		
 		// Check if there is input to be read from the keyboard
 		// If there is THEN read it, otherwise ignore the keyboard
-		// If you just readt data at it is 'EXIT' retunr from the program
+		// If you just read data at it is 'EXIT' return from the program
 		
     } catch(boost::system::system_error& e)
     {
