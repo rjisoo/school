f8faa70b843fcb9131092f58a5f321209fab7fbe
Geoffrey Corey <coreyg@onid.orst.edu>
2013-01-30 10:31:41 -0800

    squashing intial setup commits

--- homework2/myar.c
+++ homework2/myar.c
@@ -1,5 +1,31 @@
 
-int main(int argc, char *argv[]){
+/*
+ * Input:
+ * 		myar [flag] archive file_list...
+ *
+ * 		Only accepting 1 flag at a time.
+ *
+ * 		q: 	quick append to archive
+ *
+ * 		x: 	extract named files from archive
+ *
+ * 		t: 	print concise table of contents from archive
+ *
+ * 		v: 	print verbose table of contents from archive
+ *
+ * 		d: 	delete files from archive
+ *
+ * 		A: 	quick append all regular files to archive, excluding archive
+ *
+ * 		-q creates if archive doesn't exist with permissions 666
+ *
+ * 		Other commands - report error is file doesn't exist, or in wrong format
+ *
+ * 		-a and -A do not check to see if a chosen file name exist in the archive
+ *
+ * 		-x and -d operate on first file matched in archive.
+ */
+int main(int argc, char *argv[]) {
 
 	return 0;
 }
