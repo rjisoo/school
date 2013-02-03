1abe68bdfbb42abeeeef736d5bb705d78499a8a5
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-02 22:34:01 -0800

    finished -t and -v options, could possibly use cleaning up

--- homework2/myar.c
+++ homework2/myar.c
@@ -10,47 +10,43 @@
 #include <fcntl.h>
 #include <time.h>
 
-int display_usage(void);
+
 int printTable(char *pathname, int verbose);
+int octaltoascii(char ascii);
 int extract(void);
 int append(void);
 int delete(void);
+int display_usage(void);
 
 #define BUF_SIZE 4096
 #define TRUE 1
 
 int main(int argc, char *argv[]) {
-	/*
-	 * Arguments information:
-	 * 		myar == 1st arg
-	 * 		flag == 2nd arg
-	 * 		archive == 3rd arg
-	 * 		files to archive == 4+ args
-	 */
+
 //	int archiveExist, archiveNew, archiveFd, inputFd, openFlags;
-//	char flag, flagError;
+	char flag, flagError;
 //	char headerc[SARMAG];
 //	char headers[] = ARMAG;
 //	mode_t filePerms;
 //	ssize_t numRead;
 
 	/* Check to see if enough parameters supplied */
-//	if (argc < 2) {
-//		display_usage();
-//	}
+	if (argc < 2) {
+		display_usage();
+	}
 
 	/* check if flag passed and get it */
-//	flag = getopt(argc, argv, "Adqtvx");
-//	if (flag == -1){ /* No flag passed */
-//		display_usage();
-//	}
+	flag = getopt(argc, argv, "Adqtvx");
+	if (flag == -1){ /* No flag passed */
+		display_usage();
+	}
 
 	/* check if more than 1 flag passed */
-//	flagError = getopt(argc, argv, "Adqtvx");
-//	if (flagError != -1) { /* There was a wrong flag passed */
-//		printf("\nInvalid flag: %s\n\n", argv[1]);
-//		display_usage();
-//	}
+	flagError = getopt(argc, argv, "Adqtvx");
+	if (flagError != -1) { /* There was a wrong flag passed */
+		printf("\nInvalid flag: %s\n\n", argv[1]);
+		display_usage();
+	}
 
 	/* printing table of contents */
 
@@ -62,7 +58,7 @@ int main(int argc, char *argv[]) {
 	 * 					report error.
 	 */
 
-	/*switch (flag){
+	switch (flag){
 
 	case 'A':
 
@@ -77,10 +73,11 @@ int main(int argc, char *argv[]) {
 		break;
 
 	case 't':
-
+		printTable(argv[2], !TRUE);
 		break;
 
 	case 'v':
+		printTable(argv[2], TRUE);
 
 		break;
 
@@ -91,7 +88,7 @@ int main(int argc, char *argv[]) {
 	default:
 		display_usage();
 		break;
-	}*/
+	}
 
 	/* 0666 permissions */
 //	filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |S_IROTH | S_IWOTH;
@@ -119,33 +116,12 @@ int main(int argc, char *argv[]) {
 //		}
 //	}
 
-	printTable(argv[1], !TRUE);
-
-
-	exit (EXIT_SUCCESS);
-}
-
-
-int display_usage(void) {
-
-	printf(
-			"Usage: myar [-OPTION] archive-file files...\n\n"
-			"Examples:\n"
-			" myar -q archive foo bar  # Create or append archive from files foo and bar\n"
-			" myar -x archive foo bar  # Extract files foo and bar from archive\n"
-			" myar -d archive foo bar  # Delete files foo and bard from archive\n\n"
-			"Commands:\n"
-			" -A 	quickly append all regular files in current directory\n\n"
-			" -d 	delete named files form archive\n\n"
-			" -q 	quickly append named files to archive\n\n"
-			" -t 	print concise table of contents of the archive\n\n"
-			" -v 	print verbose table of contents of archive\n\n"
-			" -x 	extract named files from archive\n\n");
-	_exit(1);
-
+	//printTable(argv[2], TRUE);
 
 	return 0;
 }
+
+
 
 int printTable(char *pathname, int verbose){
 	int Fd, openFlags;
@@ -172,22 +148,121 @@ int printTable(char *pathname, int verbose){
 
 		lseek(Fd, SARMAG, SEEK_SET); /* Set it to first file name */
 
-		while (read(Fd, &ar, (size_t)sizeof(ar)) == (int)sizeof(ar)){
+		while (read(Fd, &ar, (size_t)sizeof(ar)) == sizeof(ar)){
 
-			for (int i = 0; i < 16; i++){
+			//for (int i = 0; i < 16; i++){
 				/* remove terminating / */
-				if (ar.ar_name[i] == '/'){
-					ar.ar_name[i] = '\0';
-					break;
-				}
+				//if (ar.ar_name[i] == '/'){
+					//ar.ar_name[i] = '\0';
+					//break;
+				//}
+			//}
+			int i = 0;
+			while (ar.ar_name[i] != '/'){
+				printf("%c", ar.ar_name[i]);
+				i++;
+			}
+			if (verbose != TRUE){
+				printf("\n");
+			} else {
+				printf(" 	");
 			}
 			if (verbose == TRUE){
-				/* verbose things here */
-			} else {
-				printf("%s\n", ar.ar_name);
+				/* Print visual perms */
+				octaltoascii(ar.ar_mode[3]);
+				octaltoascii(ar.ar_mode[4]);
+				octaltoascii(ar.ar_mode[5]);
+				printf(" ");
+				/* Print UID and then GID */
+				int i = 0;
+				while (i < 6){
+					printf("%c", ar.ar_uid[i]);
+					i++;
+				}
+				i = 0;
+				while (i < 6){
+					printf("%c", ar.ar_gid[i]);
+					i++;
+				}
+				/* Print file size */
+				i = 0;
+				while (i < 10){
+					printf("%c", ar.ar_size[i]);
+					i++;
+				}
+
+				/* Print humanized-time */
+				time_t time = (time_t) atol(ar.ar_date);
+				printf(" %s", ctime(&time));
 			}
 			offset = (off_t) atoi(ar.ar_size);
 			lseek(Fd, offset, SEEK_CUR);
 		}
+		if (close(Fd) == -1 ){
+			perror("close archive");
+		}
+
+	exit(EXIT_SUCCESS);
+}
+
+int octaltoascii(char ascii){
+	switch(ascii){
+	case '0':
+		printf("---");
+		break;
+
+	case '1':
+		printf("--x");
+		break;
+
+	case '2':
+		printf("-w-");
+		break;
+
+	case '3':
+		printf("-wx");
+		break;
+
+	case '4':
+		printf("r--");
+		break;
+
+	case '5':
+		printf("r-x");
+		break;
+
+	case '6':
+		printf("rw-");
+		break;
+
+	case '7':
+		printf("rwx");
+		break;
+
+	default:
+		printf("There was an error within the system.\n");
+		_exit(1);
+	}
+	return 0;
+}
+
+int display_usage(void) {
+
+	printf(
+			"Usage: myar [-OPTION] archive-file files...\n\n"
+			"Examples:\n"
+			" myar -q archive foo bar  # Create or append archive from files foo and bar\n"
+			" myar -x archive foo bar  # Extract files foo and bar from archive\n"
+			" myar -d archive foo bar  # Delete files foo and bard from archive\n\n"
+			"Commands:\n"
+			" -A 	quickly append all regular files in current directory\n\n"
+			" -d 	delete named files form archive\n\n"
+			" -q 	quickly append named files to archive\n\n"
+			" -t 	print concise table of contents of the archive\n\n"
+			" -v 	print verbose table of contents of archive\n\n"
+			" -x 	extract named files from archive\n\n");
+	_exit(1);
+
+
 	return 0;
 }
