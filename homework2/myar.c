dab2f80e2421a1f9f8a8a55e0917666326857c2c
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-04 19:00:42 -0800

    broke (read did wrong) printing functions

--- homework2/myar.c
+++ homework2/myar.c
@@ -1,4 +1,3 @@
-
 #define _POSIX_C_SOURCE 2
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +12,6 @@
 #include <fcntl.h>
 #include <libgen.h>
 #include <stdint.h>
-
 
 int printTable(char *pathname, int verbose);
 int octaltoascii(char ascii);
@@ -38,7 +36,7 @@ int main(int argc, char *argv[]) {
 
 	/* check if flag passed and get it */
 	flag = getopt(argc, argv, "Adqtvx");
-	if (flag == -1){ /* No flag passed */
+	if (flag == -1) { /* No flag passed */
 		display_usage();
 	}
 
@@ -50,7 +48,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* Otherwise check to see if a valid flag was passed and execute */
-	switch (flag){
+	switch (flag) {
 
 	case 'A':
 		if (argc < 4) {
@@ -101,11 +99,9 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-
-
-int printTable(char *pathname, int verbose){
+int printTable(char *pathname, int verbose) {
 	int Fd, openFlags;
-	char headerc[SARMAG+1], headers[] = ARMAG;
+	char headerc[SARMAG + 1], headers[] = ARMAG;
 	openFlags = O_RDONLY;
 	struct ar_hdr ar;
 	off_t offset;
@@ -113,7 +109,7 @@ int printTable(char *pathname, int verbose){
 	headerc[SARMAG] = '\0'; /* needed for strcmp */
 
 	Fd = open(pathname, openFlags);
-	if (Fd == -1){
+	if (Fd == -1) {
 		/* File doesn't exist */
 		perror(pathname);
 		_exit(EXIT_FAILURE);
@@ -122,67 +118,67 @@ int printTable(char *pathname, int verbose){
 
 	/* Check to see if it's a valid archive */
 	read(Fd, &headerc, SARMAG);
-	if (strcmp(headerc, headers) != 0){
+	if (strcmp(headerc, headers) != 0) {
 		printf("Not a valid archive type!\n");
 		_exit(EXIT_FAILURE);
 		return 1;
 	}
 
-		lseek(Fd, SARMAG, SEEK_SET); /* Set it to first file name */
+	lseek(Fd, SARMAG, SEEK_SET); /* Set it to first file name */
 
-		while (read(Fd, &ar, (size_t)sizeof(ar)) == sizeof(ar)){
+	while (read(Fd, &ar, (size_t) sizeof(ar)) == sizeof(ar)) {
 
+		int i = 0;
+		while (ar.ar_name[i] != '/') {
+			printf("%c", ar.ar_name[i]);
+			i++;
+		}
+		if (verbose != TRUE) {
+			printf("\n");
+		} else {
+			printf(" 		");
+		}
+		if (verbose == TRUE) {
+			/* Print visual perms */
+			octaltoascii(ar.ar_mode[3]);
+			octaltoascii(ar.ar_mode[4]);
+			octaltoascii(ar.ar_mode[5]);
+			printf(" ");
+			/* Print UID and then GID */
 			int i = 0;
-			while (ar.ar_name[i] != '/'){
-				printf("%c", ar.ar_name[i]);
+			while (i < 6) {
+				printf("%c", ar.ar_uid[i]);
 				i++;
 			}
-			if (verbose != TRUE){
-				printf("\n");
-			} else {
-				printf(" 		");
+			i = 0;
+			while (i < 6) {
+				printf("%c", ar.ar_gid[i]);
+				i++;
 			}
-			if (verbose == TRUE){
-				/* Print visual perms */
-				octaltoascii(ar.ar_mode[3]);
-				octaltoascii(ar.ar_mode[4]);
-				octaltoascii(ar.ar_mode[5]);
-				printf(" ");
-				/* Print UID and then GID */
-				int i = 0;
-				while (i < 6){
-					printf("%c", ar.ar_uid[i]);
-					i++;
-				}
-				i = 0;
-				while (i < 6){
-					printf("%c", ar.ar_gid[i]);
-					i++;
-				}
-				/* Print file size */
-				i = 0;
-				while (i < 10){
-					printf("%c", ar.ar_size[i]);
-					i++;
-				}
+			/* Print file size */
+			i = 0;
+			while (i < 10) {
+				printf("%c", ar.ar_size[i]);
+				i++;
+			}
 
-				/* Print humanized-time */
-				time_t time = (time_t) atol(ar.ar_date);
-				printf(" %s", ctime(&time));
-			}
-			offset = (off_t) atoi(ar.ar_size);
-			lseek(Fd, offset, SEEK_CUR);
+			/* Print humanized-time */
+			time_t time = (time_t) atol(ar.ar_date);
+			printf(" %s", ctime(&time));
 		}
-		if (close(Fd) == -1 ){
-			perror("close archive");
-			_exit(EXIT_FAILURE);
-		}
+		offset = (off_t) atoi(ar.ar_size);
+		lseek(Fd, offset, SEEK_CUR);
+	}
+	if (close(Fd) == -1) {
+		perror("close archive");
+		_exit(EXIT_FAILURE);
+	}
 
 	exit(EXIT_SUCCESS);
 }
 
-int octaltoascii(char ascii){
-	switch(ascii){
+int octaltoascii(char ascii) {
+	switch (ascii) {
 	case '0':
 		printf("---");
 		break;
@@ -226,19 +222,18 @@ int display_usage(void) {
 
 	printf(
 			"Usage: myar [-OPTION] archive-file files...\n\n"
-			"Examples:\n"
-			" myar -q archive foo bar  # Create or append archive from files foo and bar\n"
-			" myar -x archive foo bar  # Extract files foo and bar from archive\n"
-			" myar -d archive foo bar  # Delete files foo and bard from archive\n\n"
-			"Commands:\n"
-			" -A 	quickly append all regular files in current directory\n\n"
-			" -d 	delete named files form archive\n\n"
-			" -q 	quickly append named files to archive\n\n"
-			" -t 	print concise table of contents of the archive\n\n"
-			" -v 	print verbose table of contents of archive\n\n"
-			" -x 	extract named files from archive\n\n");
+					"Examples:\n"
+					" myar -q archive foo bar  # Create or append archive from files foo and bar\n"
+					" myar -x archive foo bar  # Extract files foo and bar from archive\n"
+					" myar -d archive foo bar  # Delete files foo and bard from archive\n\n"
+					"Commands:\n"
+					" -A 	quickly append all regular files in current directory\n\n"
+					" -d 	delete named files form archive\n\n"
+					" -q 	quickly append named files to archive\n\n"
+					" -t 	print concise table of contents of the archive\n\n"
+					" -v 	print verbose table of contents of archive\n\n"
+					" -x 	extract named files from archive\n\n");
 	_exit(EXIT_FAILURE);
-
 
 	return 0;
 }
@@ -255,21 +250,24 @@ int extract(int argc, char *argv[]) {
 	 * 		otherwise read archive until offset + file size
 	 * 		repeat for file list.
 	 */
-	int archiveFd;
+	int archiveFd, i, numread = 0, found = 0;
 
 	/* Open archive */
-	archiveFd = open(argv[2],O_RDONLY, 0666);
-	if (archiveFd == -1){
+	archiveFd = open(argv[2], O_RDONLY, 0666);
+	if (archiveFd == -1) {
 		perror("archive");
 		_exit(EXIT_FAILURE);
 	}
 	check_archive(archiveFd);
 
+	/* we have a valid archive */
+	lseek(archiveFd, SARMAG, SEEK_SET); /* move past ar header */
+
 	close(archiveFd);
 	return 0;
 }
 
-int append(int argc, char *argv[]){
+int append(int argc, char *argv[]) {
 	/*
 	 * Open archive file
 	 */
@@ -281,17 +279,17 @@ int append(int argc, char *argv[]){
 
 	openFlags = O_CREAT | O_RDWR;
 
-	archiveFd = open(argv[2],openFlags, 0666);
-	if (archiveFd == -1){
+	archiveFd = open(argv[2], openFlags, 0666);
+	if (archiveFd == -1) {
 		perror(argv[2]);
 		_exit(EXIT_FAILURE);
 	}
 
-	if (stat(argv[2], &sb) == -1){
+	if (stat(argv[2], &sb) == -1) {
 		perror(argv[2]);
 		_exit(EXIT_FAILURE);
 	}
-	if ((long long)sb.st_size > 0){
+	if ((long long) sb.st_size > 0) {
 		/* Existing file */
 		check_archive(archiveFd);
 
@@ -302,9 +300,9 @@ int append(int argc, char *argv[]){
 	}
 	/* we have a valid archive file, write passed files to archive */
 	/* Go through and open all files to*/
-	for (i = 0; i < argc - 3; i++){
-		if(stat(argv[i+3], &sb) == -1){
-			perror(argv[i+3]);
+	for (i = 0; i < argc - 3; i++) {
+		if (stat(argv[i + 3], &sb) == -1) {
+			perror(argv[i + 3]);
 			unlink(argv[2]);
 			_exit(EXIT_FAILURE);
 		}
@@ -312,28 +310,27 @@ int append(int argc, char *argv[]){
 	/* set to always append to end of file */
 	lseek(archiveFd, 0, SEEK_END);
 	/* all files passed exist */
-	for (i = 0; i < argc - 3; i++){
-		fileFd = open(argv[i+3], O_RDONLY);
-		if (fileFd == -1){
-			perror(argv[i+3]);
+	for (i = 0; i < argc - 3; i++) {
+		fileFd = open(argv[i + 3], O_RDONLY);
+		if (fileFd == -1) {
+			perror(argv[i + 3]);
 			unlink(argv[2]);
 			_exit(EXIT_FAILURE);
 		}
-		stat(argv[i+3], &sb);
-		sprintf(ar.ar_name, "%-16.16s", basename(argv[i+3]));
-		sprintf(ar.ar_date, "%-12u", (uint32_t)sb.st_mtime);
+		stat(argv[i + 3], &sb);
+		sprintf(ar.ar_name, "%-16.16s", basename(argv[i + 3]));
+		sprintf(ar.ar_date, "%-12u", (uint32_t) sb.st_mtime);
 		sprintf(ar.ar_uid, "%-6u", (uint32_t) sb.st_uid);
 		sprintf(ar.ar_gid, "%-6u", (uint32_t) sb.st_gid);
 		sprintf(ar.ar_mode, "%-8o", (uint32_t) sb.st_mode);
 		sprintf(ar.ar_size, "%-10u", (uint32_t) sb.st_size);
 		sprintf(ar.ar_fmag, "%s", ARFMAG);
 
-
 		write(archiveFd, (char*) &ar, sizeof(ar));
 
 		numread = 0;
 		/* Read input file */
-		while((numread = read(fileFd, buf, BUF_SIZE)) > 0){
+		while ((numread = read(fileFd, buf, BUF_SIZE)) > 0) {
 			write(archiveFd, buf, numread);
 		}
 		close(fileFd);
@@ -386,17 +383,17 @@ int append_reg(int argc, char *argv[]) {
 	return 0;
 }
 
-int check_archive(int Fd){
+int check_archive(int Fd) {
 	/* Checks first SARMAG bytes of a file to see if it's an archive type */
 	char ar_header[] = ARMAG;
 	char ar_test[SARMAG + 1];
 	ar_test[SARMAG] = '\0'; /* NULL termination accounting */
-	if (read(Fd,ar_test, SARMAG) == -1){
+	if (read(Fd, ar_test, SARMAG) == -1) {
 		perror("read");
 		_exit(EXIT_FAILURE);
 	}
 
-	if (strcmp(ar_header, ar_test) != 0){
+	if (strcmp(ar_header, ar_test) != 0) {
 		printf("Not a valid archive\n");
 		_exit(EXIT_FAILURE);
 	}
