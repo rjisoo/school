8eb7691b573fdd5ccac367abc32fb63316b6f465
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-08 11:29:00 -0800

    formatting mistake

--- homework3/uniqify.c
+++ homework3/uniqify.c
@@ -1,11 +1,11 @@
 /*
  * ##################################################################
- * # Author: 		Geoffrey Corey									#
- * # Date: 			02-08-13										#
- * # File: 			uniqify.c										#
- * # Description: 	Takes a text file and separates it into its		#
- * #				unique words and prints them on the screen 		#
- * #				using the system's /bin/sort utility. 			#
+ * # Author:        Geoffrey Corey                                  #
+ * # Date:          02-08-13                                        #
+ * # File:          uniqify.c                                       #
+ * # Description:   Takes a text file and separates it into its     #
+ * #                unique words and prints them on the screen      #
+ * #                using the system's /bin/sort utility.           #
  * ##################################################################
  */
 
