f765f7d6443b78af58e18184a09d29dc05e8cc42
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-08 11:26:06 -0800

    added initial homewokr3 files and setup the branch

---
+++ homework3/uniqify.c
@@ -0,0 +1,19 @@
+/*
+ * ##################################################################
+ * # Author: 		Geoffrey Corey									#
+ * # Date: 			02-08-13										#
+ * # File: 			uniqify.c										#
+ * # Description: 	Takes a text file and separates it into its		#
+ * #				unique words and prints them on the screen 		#
+ * #				using the system's /bin/sort utility. 			#
+ * ##################################################################
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+
+	return 0;
+}
