da0617b999175f91ebfc6ce6c6a1988987ac9300
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-10 22:12:03 -0800

    accidental need to rewrite from breaking to_lower function. need sleep

--- homework3/uniqify.c
+++ homework3/uniqify.c
@@ -1,15 +1,4 @@
-/*
- * ##################################################################
- * # Author:        Geoffrey Corey                                  #
- * # Date:          02-08-13                                        #
- * # File:          uniqify.c                                       #
- * # Description:   Takes a text file and separates it into its     #
- * #                unique words and prints them on the screen      #
- * #                using the system's /bin/sort utility.           #
- * ##################################################################
- */
-
-#define _POSIX_C_SOURCE 2
+#define _POSIX_C_SOURCE 199309
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,75 +6,79 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <malloc.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define TRUE 1
 #define FALSE 0
 #define MAX_BUF_SIZE 4096
 
-/*
- * display_usage is the error message printed to the user upon the use of invalid
- * arguments passed to the uniqify program.
- */
-int display_usage(void);
-
-/*
- * This program parses the words from the input file to be sent to the sorters
- * and uniqified.
- */
 int parser(FILE *input);
+static char *to_lower(char *str);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	int inputfd, num_proc;
+	pid_t *pids;
+	FILE *inputfile;
 
-	int inputFd, num_proc;
-	FILE *inputFile;
-
-	/* Input verification */
 	if (argc < 3) {
-		display_usage(); /* invalid number of arguments passed */
+		printf("%s [number_of_processes] inputfile\n", argv[0]);
 	}
 
-	sscanf(argv[1], "%d", &num_proc); /* get num processes */
+	sscanf(argv[1], "%d", &num_proc);
 
-	/* try to open input file */
-	inputFd = open(argv[2], O_RDONLY);
-	if (inputFd == -1) {
-		perror("Opening input file:");
+	pids = (pid_t*) calloc(num_proc, sizeof(pid_t));
+	if (pids == NULL ) {
+		perror("memory");
 		exit(EXIT_FAILURE);
 	}
 
-	inputFile = fdopen(inputFd, "r"); /* Open input file as a stream */
-
-	parser(inputFile); /* parse input file */
-
-
-	/* close input file */
-	if (fclose(inputFile) != 0) {
-		perror("input stream");
+	inputfd = open(argv[2], O_RDONLY);
+	if (inputfd == -1) {
+		perror("input file");
+		free(pids);
 		exit(EXIT_FAILURE);
 	}
 
+	inputfile = fdopen(inputfd, "r");
+
+	parser(inputfile);
+
+	if (fclose(inputfile) != 0) {
+		perror("closing input stream");
+		free(pids);
+		exit(EXIT_FAILURE);
+	}
+
+	free(pids);
 	exit(EXIT_SUCCESS);
 }
 
-int display_usage(void) {
-	printf("\n"
-			"uniqify:\n  A parallel sorting process that displays the "
-			"unique words contained in a text file.\n\n"
-			"Usage:\n  uniqify  [num_processes] inputfile\n"
-			"\n"
-			"Flags:\n"
-			"  -h \tPrintf help message.\n\n");
-
-	exit(EXIT_FAILURE);
-}
-
-int parser(FILE *input) {
-
+int parser(FILE *file)
+{
 	char buffer[200];
 
-	while( fscanf(input, "%*[^A-Za-z]"), fscanf(input, "%198[a-zA-Z]", buffer) > 0){
+	while (fscanf(file, "%*[^A-Za-z]"), fscanf(file, "%198[a-zA-Z]", buffer)
+	                > 0) {
 		strcat(buffer, "\n");
-		printf("%s", buffer);
+		printf("%s", to_lower(buffer));
 	}
 	return 0;
+}
+
+/* ttp://stackoverflow.com/questions/6857445/lowercase-urls-in-varnish-inline-c */
+static char *to_lower(char *str)
+{
+	char *s = str;
+	while (*s) {
+		if (isupper (*s)) {
+			*s = tolower (*s);
+		}
+		s++;
+	}
+	return str;
 }
