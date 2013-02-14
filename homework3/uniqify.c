dc11b3d388ad73e200cda8696da032a754a72bbe
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-13 17:08:39 -0800

    got piping to 1 sort process working right now, it uses sorts builting unique function, remember to remove that, also got pipe back to uniqify working.
    Need to: setup for multiple sort processes, and thentackle suppressor.

--- homework3/uniqify.c
+++ homework3/uniqify.c
@@ -7,8 +7,11 @@
 #include <ctype.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <wait.h>
 
 struct sigaction act;
+pid_t pid;
 
 int parser(FILE *input, FILE *output);
 static char *to_lower(char *str);
@@ -16,37 +19,82 @@ void signalHandler(int signum, siginfo_t *info, void *ptr);
 
 int main(int argc, char *argv[])
 {
+
+	int num_proc;
+	int pipefdout[2], pipefdin[2];
+	FILE *output, *input;
+
+	if (pipe(pipefdout) != 0) {
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pipe(pipefdin) != 0) {
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+
+	/* check if correct number of arguments passed */
 	if (argc < 2) {
 		printf("\n Usage: uniqify -[number_of_processes]\n");
 		exit(EXIT_FAILURE);
 	}
-	int num_proc;
-	sscanf(argv[1] + 1, "%d", &num_proc);
+
+	/* read in number of processes desired */
+	sscanf(argv[1] + 1, "%d", &num_proc); /* sneaky hack to remove - */
 	if (num_proc == 0) {
-		printf(
-		                "\n Usage: uniqify -[number_of_processes (greater than zero)]\n");
+		printf("\n Usage: uniqify -[number_of_processes (greater than zero)]\n");
 		exit(EXIT_FAILURE);
 	}
 	printf("\nNumber of commands: %d, Number of processes: %d\n", argc,
 	                num_proc);
-	memset(&act, 0, sizeof(act));
 
+	/* Setting up signal handling */
+	memset(&act, 0, sizeof(act));
 	act.sa_sigaction = signalHandler;
 	act.sa_flags = SA_SIGINFO;
 	sigaction(SIGINT, &act, NULL );
 	sigaction(SIGQUIT, &act, NULL );
 	sigaction(SIGHUP, &act, NULL );
 
-	while (parser(stdin, stdout) > 0) {
-		//sleep(50);
+	switch(pid = fork()){
+	case -1:
+		perror("process creation");
+		exit(EXIT_FAILURE);
+	case 0:
+		//child
+		close(pipefdout[1]);
+		close(pipefdin[0]);
+		dup2(pipefdout[0], STDIN_FILENO);
+		dup2(pipefdin[1], STDOUT_FILENO);
+		close(pipefdout[0]);
+		close(pipefdin[1]);
+		execlp("sort", "sort", "-u", (char *)NULL);
+
+		break;
+	default:
+		output = fdopen(pipefdout[1], "w");
+		input = fdopen(pipefdin[0], "r");
+
+		close(pipefdout[0]);
+		close(pipefdin[1]);
+		while(parser(stdin, output) > 0);
+		fclose(output);
+		while(parser(input, stdout) > 0);
+		fclose(input);
+		break;
 	}
+
 
 	exit(EXIT_SUCCESS);
 }
 
 void signalHandler(int signum, siginfo_t *info, void *ptr)
 {
-	printf("\nSignal: %d\n", signum);
+	int status;
+	//printf("\nSignal: %d\n", signum);
+	kill(pid, signum);
+	wait(&status);
 	exit(EXIT_FAILURE);
 }
 
