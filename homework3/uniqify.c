39a05097e130edf231664a0fcf2dd0159ed15591
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-15 11:08:22 -0800

    rewrote uniqify for better error handling, better readability, have not implemented the parser yet in the rewrite. the old uinqify is no uniqifyold1.c

--- homework3/uniqify.c
+++ homework3/uniqify.c
@@ -1,103 +1,32 @@
 #define _POSIX_C_SOURCE 199309
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
-#include <ctype.h>
-#include <string.h>
-#include <signal.h>
+#include <malloc.h>
 #include <sys/types.h>
 #include <wait.h>
-#include <malloc.h>
+#include <signal.h>
+#include <string.h>
 
+pid_t *pids; /* These need to be here global for use with the signal handler */
+int process_num;
 struct sigaction act;
-//int *pid, num_proc; /* This is a global variable for usage with the signal handler */
-pid_t *pids;
-long num_proc;
 
-int parser(FILE *input, FILE *output);
-static char *to_lower(char *str);
 void signalHandler(int signum, siginfo_t *info, void *ptr);
+int input_validation(int argc, char *argv[]);
 void display_error(void);
-
 
 int main(int argc, char *argv[])
 {
+	/* Variables */
+	int i, exit_num;
+	char mem_err[] = "Memory allocation failed";
+	char stream_err[] = "Stream allocation failed";
 
-	int i; /* The number of processes to run */
-	int **pipefdin; /* array of file descriptors for input from sorter */
-	int **pipefdout; /* array of file descriptors for output to sorter */
-	//int pipefdout[2], pipefdin[2];
-	//FILE *output, *input;
-	FILE **output;
-	FILE **input;
-	char mem_err[] = "Memory allocation failure";
-	char stream_err[] = "Stream allocation failure";
-
-	/* check if correct number of arguments passed */
-	if (argc < 2) {
-		display_error();
-		exit(EXIT_FAILURE);
-	}
-
-	/* read in number of processes desired */
-	sscanf(argv[1]+1, "%ld", &num_proc); /* sneaky hack to remove - */
-	if (num_proc == 0) {
-		display_error();
-		exit(EXIT_FAILURE);
-	}
-
-	/* Check to see if number of desired processes is greater than the system
-	 * allows.
-	 */
-	if(num_proc > sysconf(_SC_CHILD_MAX)){
-		fprintf(stderr, "Sorry, %ld is too large for this system\n", num_proc);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Allocating array to hold pipe file descriptors */
-	pipefdin = (int**)calloc(num_proc, sizeof(int*));
-	if(pipefdin == NULL){
-		fprintf(stderr, "%s\n", mem_err);
-		exit(EXIT_FAILURE);
-	}
-
-	pipefdout = (int**) calloc(num_proc, sizeof(int*));
-	if (pipefdout == NULL ) {
-		fprintf(stderr, "%s\n", mem_err);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Allocate stream */
-	input = (FILE**)calloc(num_proc, sizeof(FILE*));
-	if(input == NULL){
-		fprintf(stderr, "%s\n", stream_err);
-		exit(EXIT_FAILURE);
-	}
-	output = (FILE**) calloc(num_proc, sizeof(FILE*));
-	if (output == NULL ) {
-		fprintf(stderr, "%s\n", stream_err);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Allocate space for pipe file descriptors to store */
-	for(i = 0; i < num_proc; i++){
-		pipefdout[i] = (int*)calloc(2, sizeof(int));
-		if(pipefdout[i] == NULL){
-			fprintf(stderr, "%s\n", mem_err);
-			exit(EXIT_FAILURE);
-		}
-		pipefdin[i] = (int*)calloc(2, sizeof(int));
-		if(pipefdin[i] == NULL){
-			fprintf(stderr, "%s\n", mem_err);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	/* Allocate array of pids */
-	pids = (pid_t*)calloc(num_proc, sizeof(pid_t));
-
+	int **pipefdin, **pipefdout; /* for piping/plumbing to sort */
+	FILE **input, **output; /* for giving/getting data to/from sort */
 
 	/* Setting up signal handling */
 	memset(&act, 0, sizeof(act));
@@ -107,90 +36,120 @@ int main(int argc, char *argv[])
 	sigaction(SIGQUIT, &act, NULL );
 	sigaction(SIGHUP, &act, NULL );
 
-	/* Begin piping and forking processes */
-	for (i = 0; i < num_proc; i++) {
-		if (pipe(pipefdin[i]) == -1) {
-			perror("pipes");
-			exit(EXIT_FAILURE);
-		}
-		if (pipe(pipefdout[i]) == -1) {
-			perror("pipes");
-			exit(EXIT_FAILURE);
-		}
-		switch (pids[i] = fork()) {
-		case -1:
-			perror("process not created");
-			free(pipefdin);
+	/* This makes exiting the piping, forking, and plumbing section cleaner */
+	exit_num = EXIT_SUCCESS;
+
+	process_num = input_validation(argc, argv);
+
+	/* allocate pipes holding pipes */
+	pipefdin = (int**) calloc(process_num, sizeof(int*));
+	pipefdout = (int**) calloc(process_num, sizeof(int*));
+	if (pipefdin == NULL || pipefdout == NULL ) {
+		fprintf(stderr, "%s\n", mem_err);
+		exit(EXIT_FAILURE);
+	}
+	/* Allocate pipes */
+	for (i = 0; i < process_num; i++) {
+		pipefdin[i] = (int*) calloc(2, sizeof(int));
+		pipefdout[i] = (int*) calloc(2, sizeof(int));
+		if (pipefdin[i] == NULL || pipefdout[i] == NULL ) {
+			fprintf(stderr, "%s\n", mem_err);
 			free(pipefdout);
-			free(input);
-			free(output);
-			break;
+			free(pipefdin);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	/* allocate file streams */
+	input = (FILE**) calloc(process_num, sizeof(FILE*));
+	output = (FILE**) calloc(process_num, sizeof(FILE*));
+	if (input == NULL || output == NULL ) {
+		fprintf(stderr, "%s\n", stream_err);
+		free(input);
+		free(output);
+		free(pipefdin);
+		free(pipefdout);
+		exit(EXIT_FAILURE);
+	}
+
+	/* Allocate holder for PIDs */
+	pids = (pid_t*) calloc(process_num, sizeof(pid_t));
+
+	/* pipe, fork, and plumb */
+	for (i = 0; i < process_num; i++) {
+		if (pipe(pipefdin[i]) == -1 || pipe(pipefdout[i]) == -1) {
+			perror("pipes");
+			exit(EXIT_FAILURE);
+			/*
+			 * hacky, cleans up the main section of code for
+			 * readability though
+			 */
+		}
+		/* Create processes */
+		switch (pids[i] = fork()) {
+
+		case -1:
+			/* error */
+			perror("process not created");
+			exit(EXIT_FAILURE);
 
 		case 0:
-			/* child */
-			close(pipefdout[i][1]); /* close sort write fd */
-			close(pipefdin[i][0]); /* close sort out's read fd */
-			dup2(pipefdout[i][0], STDIN_FILENO); /* set sort in as parent out */
-			dup2(pipefdin[i][1], STDOUT_FILENO);/* set sort out as output */
-			close(pipefdout[i][0]); /* close it's remapped fd */
-			close(pipefdin[i][1]); /* close it's remapped fd */
+			/* Child stuff and plumbing */
+			/* close sort write fd */
+			if (close(pipefdout[i][1]) == -1) {
+				perror("close sort in write");
+				_exit(EXIT_FAILURE);
+			}
+			/* close sort out's read fd */
+			if (close(pipefdin[i][0]) == -1) {
+				perror("close sort out read");
+				_exit(EXIT_FAILURE);
+			}
+			/* set sort in as parent out */
+			if (dup2(pipefdout[i][0], STDIN_FILENO) == -1) {
+				perror("mapping sort stdin");
+				_exit(EXIT_FAILURE);
+			}
+			/* set sort out as output */
+			if (dup2(pipefdin[i][1], STDOUT_FILENO) == -1) {
+				perror("mapping sort stdout");
+				_exit(EXIT_FAILURE);
+			}
+			/* close it's remapped fd */
+			if (close(pipefdout[i][0]) == -1) {
+				perror(
+				                "closing sort unused pipe in from remap");
+				_exit(EXIT_FAILURE);
+			}
+			/* close it's remapped fd */
+			if (close(pipefdin[i][1]) == -1) {
+				perror(
+				                "closing sort unused pipe out from remap");
+				_exit(EXIT_FAILURE);
+			}
 			execlp("sort", "sort", (char *) NULL );
 			break;
 
 		default:
-			/* parent */
+			/* Parent stuff and plumbing */
 			output[i] = fdopen(pipefdout[i][1], "w");
 			input[i] = fdopen(pipefdin[i][0], "r");
-
 			close(pipefdout[i][0]);
 			close(pipefdin[i][1]);
 			break;
 		}
 	}
+	sleep(50);
 
-	//sleep(50);
-
-	/* parse */
-	i = 0;
-	while (parser(stdin, output[i]) > 0) {
-		if (i == num_proc - 1) {
-			i = 0;
-		} else {
-			i++;
-		}
-	}
-	for(i = 0; i< num_proc; i++){
+	for (i = 0; i < process_num; i++) {
 		fclose(output[i]);
-	}
-	//while (parser(stdin, output[0]) > 0);
-	//fclose(output[0]);
-	i = 0;
-	while(parser(input[i], stdout) > 0){
-		if(i == num_proc - 1){
-			i = 0;
-		} else {
-			i++;
-		}
-	}
-	//while (parser(input[0], stdout) > 0);
-	//fclose(input[0]);
-	for(i = 0; i < num_proc; i++){
 		fclose(input[i]);
 	}
-
-	/* cleanup the arrays */
-	free(pipefdin);
-	free(pipefdout);
 	free(input);
 	free(output);
-
-	exit(EXIT_SUCCESS);
-}
-
-void display_error(void)
-{
-	char error[] = "Usage: uniqify -[number_of_processes (greater than zero)]";
-	fprintf(stderr, "\n %s\n\n\n", error);
+	free(pipefdin);
+	free(pipefdout);
+	exit(exit_num);
 }
 
 void signalHandler(int signum, siginfo_t *info, void *ptr)
@@ -199,39 +158,38 @@ void signalHandler(int signum, siginfo_t *info, void *ptr)
 	//printf("\nSignal: %d\n", signum);
 	//kill(pids[0], signum);
 	//wait(&status);
-	for(i = 0; i < num_proc; i++){
-		kill(pids[i], signum);
+	for (i = 0; i < process_num; i++) {
+		kill(pids[i], SIGKILL);
 		wait(&status);
 	}
 	exit(EXIT_FAILURE);
 }
 
-int parser(FILE *input, FILE *output)
+int input_validation(int argc, char *argv[])
 {
-
-	char buffer[200];
-
-	int read;
-
-	read = (fscanf(input, "%*[^A-Za-z]"), fscanf(input, "%198[a-zA-Z]",
-	                buffer));
-
-	if (read > 0) {
-		strcat(buffer, "\n");
-		fprintf(output, "%s", to_lower(buffer));
+	int process_num;
+	/* Check that only number of processes is passed as argument */
+	if (argc < 2 || argc > 2) {
+		display_error();
+		exit(EXIT_FAILURE);
 	}
-	return read;
+
+	/* Read in the number of processes wanted */
+	sscanf(argv[1] + 1, "%d", &process_num); /* sneaky pointer math to remove - */
+	if (process_num == 0) {
+		display_error();
+		exit(EXIT_FAILURE);
+	} else if (process_num > sysconf(_SC_CHILD_MAX)) { /* sanity check */
+		fprintf(stderr, "Sorry, %d is too large for this system\n",
+		                process_num);
+		exit(EXIT_FAILURE);
+	}
+	return process_num;
 }
 
-/* http://stackoverflow.com/questions/6857445/lowercase-urls-in-varnish-inline-c */
-static char *to_lower(char *str)
+void display_error(void)
 {
-	char *s = str;
-	while (*s) {
-		if (isupper(*s)) {
-			*s = tolower(*s);
-		}
-		s++;
-	}
-	return str;
+	char error[] =
+	                "Usage: uniqify -[number_of_processes (greater than zero)]";
+	fprintf(stderr, "\n %s\n\n\n", error);
 }
