7b5a1ebe710989d335a1730374ab27e3fa8c36ee
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-18 21:16:34 -0800

    rewrote in c++ to take advantage of easier string handling/dynamic strings

--- homework3/uniqify.cpp
+++ homework3/uniqify.cpp
@@ -18,29 +18,68 @@ using std::cout;
 #include <vector>
 
 #define MAX_STR_LEN 200
+#define EXIT_STR "zzzzzzz"
 
-void wait_for(int processes);
-void suppressor(int processes, int **fds);
+void reap(int processes);
+void suppressor(int processes, int fds[][2]);
+void suppress(int processes, int to_sort[][2], int fr_sort[][2]);
 void close_file(int processes, FILE **streams);
 int get_arguments(int argc, char *argv[]);
-void create_pipe(int processes, int **toSort, int **fromSort);
-void sorter(int processes, int **toSort, int **fromSort);
-void parser(int processes, int **toSort, int **fromSort);
-void read_pipe(int position, std::vector<std::string> &cache, FILE **BACK, int *timeout);
+void create_pipe(int processes, int toSort[][2], int fromSort[][2]);
+void sorter(int processes, pid_t *pids, int toSort[][2], int fromSort[][2]);
+void parser(int processes, int toSort[][2], int fromSort[][2]);
+void read_pipe(int position, std::vector<std::string> &temp, FILE **input, int *timeout);
 int get_word(char *word);
 static char *to_lower(char *str);
 void signalHandler(int signum, siginfo_t *info, void *ptr);
 
-pid_t *pids; /* needed to be a global variable for signal handling */
+
+struct sigaction act;
 int processes;
+pid_t *pidfor_signal;
 
 int main(int argc, char *argv[])
 {
 
+	//fprintf(stderr, "getting num proc\n");
+	processes = get_arguments(argc, argv);
+
+
+	//fprintf(stderr, "making pipe arrays\n");
+	int toSort[processes][2];
+	int fromSort[processes][2];
+	pid_t pids[processes];
+	pidfor_signal = pids;
+
+	/* Setting up signal handling */
+	//fprintf(stderr, "setting up signal\n");
+	memset(&act, 0, sizeof(act));
+	act.sa_sigaction = signalHandler;
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGINT, &act, NULL );
+	sigaction(SIGQUIT, &act, NULL );
+	sigaction(SIGHUP, &act, NULL );
+
+
+	//fprintf(stderr, "creating pipes\n");
+	create_pipe(processes, toSort, fromSort);
+
+	//fprintf(stderr, "sorting\n");
+	sorter(processes, pidfor_signal, toSort, fromSort);
+
+	//fprintf(stderr, "suppress\n");
+	suppress(processes, toSort, fromSort);
+
+	//fprintf(stderr, "parsing\n");
+	parser(processes, toSort, fromSort);
+
+	//fprintf(stderr, "reaping\n");
+	reap(processes);
+
 	exit(EXIT_SUCCESS);
 }
 
-void wait_for(int process)
+void reap(int process)
 {
 	int status;
 	for (int i = 0; i < process; i++) {
@@ -48,9 +87,80 @@ void wait_for(int process)
 	}
 }
 
-void suppressor(int processes, int **fds)
+void suppressor(int processes, int fds[][2])
 {
+	std::vector<std::string> temp;
+	std::string check, past = " ";
+	int valid, lowest, timeout, i;
+	FILE **input;
 
+	valid = 1;
+	lowest = 0;
+	timeout = 0;
+
+	input = (FILE**)calloc(processes, sizeof(FILE*));
+	if(input == NULL){
+		perror("stream from sort allocation");
+		exit(EXIT_FAILURE);
+	}
+
+	/* open all the relavent input streams from sorter processes */
+	for (i = 0; i < processes; i++){
+		input[i] = fdopen(fds[i][0], "r");
+	}
+
+
+	for (i = 0; i < processes; i++){
+		temp.push_back(" ");
+		read_pipe(i, temp, input, &timeout);
+	}
+
+	while(valid && timeout < processes){
+		valid = 0;
+		check = EXIT_STR;
+
+		for (i = 0; i < processes; i++){
+			if(temp[i] != EXIT_STR){
+				valid++;
+				if (temp[i] < check) {
+					check = temp[i];
+					lowest = i;
+				}
+			}
+		}
+		if ((check != past) && (check != EXIT_STR)) {
+			fputs(check.c_str(), stdout);
+			past = check;
+		}
+		read_pipe(lowest, temp, input, &timeout);
+	}
+	close_file(processes, input);
+}
+
+void suppress(int processes, int toSort[][2], int fromSort[][2])
+{
+	int i;
+	switch(fork()){
+
+	case -1: /* error making child */
+		perror("error making suppressor child");
+		_exit(EXIT_FAILURE);
+
+	case 0: /* child created */
+		/* Cleanup all copies of unneeded FDs */
+		for ( i = 0; i < processes; i++){
+			close(toSort[i][0]);
+			close(toSort[i][1]);
+			close(fromSort[i][1]);
+		}
+		/* Suppress now */
+		suppressor(processes, fromSort);
+		_exit(EXIT_SUCCESS);
+
+	default: /* Parent stuff */
+		break;
+
+	}
 }
 
 void close_file(int processes, FILE **streams)
@@ -73,11 +183,15 @@ int get_arguments(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	} else {
 		sscanf(argv[1] + 1, "%d", &process_number);
+		if (process_number == 0){
+			fprintf(stderr,  "Usage: uniqify -[number > 0]\n");
+			exit(EXIT_FAILURE);
+		}
 	}
 	return process_number;
 }
 
-void create_pipe(int processes, int **toSort, int **fromSort)
+void create_pipe(int processes, int toSort[][2], int fromSort[][2])
 {
 
 	int i;
@@ -93,57 +207,102 @@ void create_pipe(int processes, int **toSort, int **fromSort)
 	}
 }
 
-void sorter(int processes, int **toSort, int **fromSort)
+void sorter(int processes, pid_t *pids, int toSort[][2], int fromSort[][2])
 {
 
+	int i, j;
+
+	for (i = 0; i < processes; i++) {
+		switch (pids[i] = fork()) {
+
+		case -1: /* Error making shild */
+			perror("creating child");
+			exit(EXIT_FAILURE);
+
+		case 0: /* Setup child before exec */
+			/* Cleanup all unneeded FDs */
+			for (j = 0; j < processes; j++) {
+				close(toSort[j][1]);
+				close(fromSort[j][0]);
+				if (j == i) {
+					dup2(toSort[j][0], STDIN_FILENO);
+					dup2(fromSort[j][1], STDOUT_FILENO);
+				} else {
+					close(toSort[j][0]);
+					close(fromSort[j][1]);
+				}
+			}
+			execlp("sort", "sort", (char*) NULL);
+
+		default:
+			break;
+		}
+	}
 }
 
-void parser(int processes, int **toSort, int **fromSort)
+void parser(int processes, int toSort[][2], int fromSort[][2])
 {
 
 	FILE **output;
-	char word[200];
+	char word[MAX_STR_LEN];
 	int i;
 
 	output = (FILE**) calloc(processes, sizeof(FILE*));
 
-	if (output == NULL ) {
+	if (output == NULL) {
 		perror("file stream allocation");
 		exit(EXIT_FAILURE);
 	}
 
+	/* close unneeded FDs and end of pipes */
 	for (i = 0; i < processes; i++) {
 		close(toSort[i][0]);
 		close(fromSort[i][0]);
 		close(fromSort[i][1]);
 		output[i] = fdopen(toSort[i][1], "w");
 	}
+
+	/* read words from stdin and put it to sorter */
 	i = 0;
-	while (get_word(word) > 0) {
+	while (get_word(word) != EOF) {
 		fputs(word, output[i]);
-		if (i == processes - 1) {
+		if(i == processes - 1){
 			i = 0;
 		} else {
 			i++;
 		}
 	}
+	/* Cleanup of input streams to sort */
 	close_file(processes, output);
 }
 
 int get_word(char *word)
 {
 
-	int length = 0;
-	if (fscanf(stdin, "%198[A-Za-z']%*[^A-Za-z']", word) > 0) {
+	int read;
+	if ((read = fscanf(stdin, "%198[A-Za-z]%*[^A-Za-z]", word)) != EOF) {
 		strcat(word, "\n");
 		to_lower(word);
-		length = strlen(word);
+		return read;
+	} else {
+		return EOF;
 	}
-	return length;
 }
 
-void read_pipe(int position, std::vector<std::string> &cache, FILE **BACK, int *timeout) {
+void read_pipe(int position, std::vector<std::string> &temp, FILE **input,
+                int *timeout)
+{
 
+	char word[MAX_STR_LEN];
+
+	if (!feof(input[position]) &&
+	    !ferror(input[position]) &&
+	     fgets(word, MAX_STR_LEN, (input[position])) != NULL) {
+		temp[position] = word;
+	} else {
+		++(*timeout);
+		temp[position] = EXIT_STR;
+	}
 }
 
 /* http://stackoverflow.com/questions/6857445/lowercase-urls-in-varnish-inline-c */
@@ -166,7 +325,7 @@ void signalHandler(int signum, siginfo_t *info, void *ptr)
 	//kill(pids[0], signum);
 	//wait(&status);
 	for (i = 0; i < processes; i++) {
-		kill(pids[i], SIGKILL);
+		kill(pidfor_signal[i], SIGKILL);
 		wait(&status);
 	}
 	exit(EXIT_FAILURE);
