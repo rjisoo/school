0f3a50924bb90ea5ff707f215508a9eb064e53f5
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-18 18:58:11 -0800

    rewrote program using c++ for easier usage

--- homework3/uniqify.c
+++ homework3/uniqify.c
@@ -1,5 +1,9 @@
 #define _POSIX_C_SOURCE 199309
 
+#include <iostream>
+using std::cin;
+using std::cout;
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,320 +14,136 @@
 #include <signal.h>
 #include <string.h>
 #include <ctype.h>
+#include <string>
+#include <vector>
 
-pid_t *pids; /* These need to be here global for use with the signal handler */
-int process_num;
-struct sigaction act;
+#define MAX_STR_LEN 200
 
-void signalHandler(int signum, siginfo_t *info, void *ptr);
-int input_validation(int argc, char *argv[]);
-int parseToSort(FILE *input, FILE *output);
-int readFromSort(FILE *to, FILE *from);
+void wait_for(int processes);
+void suppressor(int processes, int **fds);
+void close_file(int processes, FILE **streams);
+int get_arguments(int argc, char *argv[]);
+void create_pipe(int processes, int **toSort, int **fromSort);
+void sorter(int processes, int **toSort, int **fromSort);
+void parser(int processes, int **toSort, int **fromSort);
+void read_pipe(int position, std::vector<std::string> &cache, FILE **BACK, int *timeout);
+int get_word(char *word);
 static char *to_lower(char *str);
-void display_error(void);
+void signalHandler(int signum, siginfo_t *info, void *ptr);
+
+pid_t *pids; /* needed to be a global variable for signal handling */
+int processes;
 
 int main(int argc, char *argv[])
 {
-	/* Variables */
-	int i, j;
-	char mem_err[] = "Memory allocation failed";
-	char stream_err[] = "Stream allocation failed";
 
-	int **pipesortin, **pipesortout; /* for piping/plumbing to sort */
-	FILE **input, **output; /* for giving/getting data to/from sort */
-
-	int pipeToSuppress[2], pipeFromSuppress[2]; /* Pipes to/from suppressor */
-	FILE *toSuppress, *fromSuppress; /* Stream to/from suppressor */
-
-	pid_t suppressor;
-
-	/* Setting up signal handling */
-	memset(&act, 0, sizeof(act));
-	act.sa_sigaction = signalHandler;
-	act.sa_flags = SA_SIGINFO;
-	sigaction(SIGINT, &act, NULL );
-	sigaction(SIGQUIT, &act, NULL );
-	sigaction(SIGHUP, &act, NULL );
-
-
-	process_num = input_validation(argc, argv);
-
-	/* allocate pipes holding pipes */
-	pipesortin = (int**) calloc(process_num, sizeof(int*));
-	pipesortout = (int**) calloc(process_num, sizeof(int*));
-	if (pipesortin == NULL || pipesortout == NULL ) {
-		fprintf(stderr, "%s\n", mem_err);
-		exit(EXIT_FAILURE);
-	}
-	/* Allocate pipes */
-	for (i = 0; i < process_num; i++) {
-		pipesortin[i] = (int*) calloc(2, sizeof(int));
-		pipesortout[i] = (int*) calloc(2, sizeof(int));
-		if (pipesortin[i] == NULL || pipesortout[i] == NULL ) {
-			fprintf(stderr, "%s\n", mem_err);
-			free(pipesortout);
-			free(pipesortin);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	/* allocate file streams */
-	input = (FILE**) calloc(process_num, sizeof(FILE*));
-	output = (FILE**) calloc(process_num, sizeof(FILE*));
-	if (input == NULL || output == NULL ) {
-		fprintf(stderr, "%s\n", stream_err);
-		free(input);
-		free(output);
-		free(pipesortin);
-		free(pipesortout);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Allocate holder for PIDs */
-	pids = (pid_t*) calloc(process_num, sizeof(pid_t));
-
-	/* pipe and plumb */
-	for (i = 0; i < process_num; i++) {
-		if (pipe(pipesortin[i]) == -1 || pipe(pipesortout[i]) == -1) {
-			perror("sort pipes");
-			exit(EXIT_FAILURE);
-		}
-
-		/* Create processes (fork) */
-		switch (pids[i] = fork()) {
-
-		case -1:
-			/* error */
-			perror("process not created");
-			exit(EXIT_FAILURE);
-
-		case 0:
-			/* Child stuff and plumbing */
-			/* close sort write fd */
-			if (close(pipesortout[i][1]) == -1) {
-				perror("close sort in write");
-				_exit(EXIT_FAILURE);
-			}
-			/* close sort out's read fd */
-			if (close(pipesortin[i][0]) == -1) {
-				perror("close sort out read");
-				_exit(EXIT_FAILURE);
-			}
-			/* set sort in as parent out */
-			if (dup2(pipesortout[i][0], STDIN_FILENO) == -1) {
-				perror("mapping sort stdin");
-				_exit(EXIT_FAILURE);
-			}
-			/* set sort out as output */
-			if (dup2(pipesortin[i][1], STDOUT_FILENO) == -1) {
-				perror("mapping sort stdout");
-				_exit(EXIT_FAILURE);
-			}
-			/* close it's remapped fd */
-			if (close(pipesortout[i][0]) == -1) {
-				perror(
-				                "closing sort unused pipe in from remap");
-				_exit(EXIT_FAILURE);
-			}
-			/* close it's remapped fd */
-			if (close(pipesortin[i][1]) == -1) {
-				perror(
-				                "closing sort unused pipe out from remap");
-				_exit(EXIT_FAILURE);
-			}
-			if(execlp("sort", "sort", (char *) NULL ) == -1){
-				perror("sort exec failed");
-				_exit(EXIT_FAILURE);
-			}
-			break;
-
-		default:
-			/* Parent stuff and plumbing */
-			output[i] = fdopen(pipesortout[i][1], "w");
-			input[i] = fdopen(pipesortin[i][0], "r");
-			if (output[i] == NULL || input[i] == NULL ) {
-				perror("stream to sort");
-				exit(EXIT_FAILURE);
-			}
-			if (close(pipesortout[i][0]) == -1) {
-				perror("closing sort unneeded fd");
-				exit(EXIT_FAILURE);
-			}
-			if (close(pipesortin[i][1]) == -1) {
-				perror("closing unneeded fd");
-				exit(EXIT_FAILURE);
-			}
-			break;
-		}
-	}
-
-	/* Parse input from stdin to sort */
-	i = 0;
-	while (parseToSort(stdin, output[i]) != EOF) {
-		if (i + 1 == process_num) {
-			i = 0;
-		} else {
-			i++;
-		}
-	}
-	fclose(stdin);
-	for (i = 0; i < process_num; i++) {
-		fclose(output[i]);
-	}
-
-
-	/* Pipe the suppressor process */
-	if (pipe(pipeToSuppress) == -1 || pipe(pipeFromSuppress) == -1) {
-		perror("piping suppressor");
-		exit(EXIT_FAILURE);
-	}
-
-	/* create the suppressor process */
-	switch (suppressor = fork()) {
-
-	case -1: /* Error creating child */
-		perror("suppressor process error");
-		exit(EXIT_FAILURE);
-
-	case 0: /* Child process setup */
-		/* Close suppress in write fd */
-		if (close(pipeToSuppress[1]) == -1) {
-			perror("closing suppress in write fd");
-			_exit(EXIT_FAILURE);
-		}
-		/*Close suppress out read fd */
-		if (close(pipeFromSuppress[0]) == -1) {
-			perror("closing suppress out read fd");
-			_exit(EXIT_FAILURE);
-		}
-		/* Set suppressor in as parent out */
-		if (dup2(pipeToSuppress[0], STDIN_FILENO) == -1) {
-			perror("mapping suppressor in as parent out");
-			_exit(EXIT_FAILURE);
-		}
-		/* Set suppressor out as parent in */
-		if (dup2(pipeFromSuppress[1], STDOUT_FILENO) == -1) {
-			perror("mapping suppressor out as parent in");
-			_exit(EXIT_FAILURE);
-		}
-		/* Cleanup the unused pipe FDs */
-		if (close(pipeToSuppress[0]) == -1
-		                || close(pipeFromSuppress[1]) == -1) {
-			perror("closing old pipe FDs");
-			_exit(EXIT_FAILURE);
-		}
-		/* exec suppressor executable */
-		if(execl("./suppressor", "suppressor", NULL) == -1){
-			perror("failed to exec suppressor");
-			_exit(EXIT_FAILURE);
-		}
-		break;
-
-	default: /* plumb for the parent side */
-		/* Open file streams to/from suppressor */
-		toSuppress = fdopen(pipeToSuppress[1], "w");
-		fromSuppress = fdopen(pipeFromSuppress[0], "r");
-		if (toSuppress == NULL || fromSuppress == NULL ) {
-			perror("stream for suppressor");
-			exit(EXIT_FAILURE);
-		}
-		/* Close unneeded FDs */
-		if (close(pipeToSuppress[1]) == -1) {
-			perror("closing suppressor to pipe");
-			exit(EXIT_FAILURE);
-		}
-		if (close(pipeFromSuppress[0]) == -1) {
-			perror("closing suppressor from pipe");
-			exit(EXIT_FAILURE);
-		}
-		break;
-	}
-
-	/* Read from sorters round-robin, and suppress (for now, print to stdout) */
-
-	i = 0;
-	while (readFromSort(stdout, input[i]) != EOF) {
-		if (i + 1 == process_num) {
-			i = 0;
-		} else {
-			i++;
-		}
-	}
-
-	for (i = 0; i < process_num; i++) {
-		fclose(input[i]);
-	}
-
-	free(input);
-	free(output);
-	free(pipesortin);
-	free(pipesortout);
 	exit(EXIT_SUCCESS);
 }
 
-void signalHandler(int signum, siginfo_t *info, void *ptr)
+void wait_for(int process)
 {
-	int status, i;
-	//printf("\nSignal: %d\n", signum);
-	//kill(pids[0], signum);
-	//wait(&status);
-	for (i = 0; i < process_num; i++) {
-		kill(pids[i], SIGKILL);
+	int status;
+	for (int i = 0; i < process; i++) {
 		wait(&status);
 	}
-	exit(EXIT_FAILURE);
 }
 
-int input_validation(int argc, char *argv[])
+void suppressor(int processes, int **fds)
 {
-	int process_num;
-	/* Check that only number of processes is passed as argument */
-	if (argc < 2 || argc > 2) {
-		display_error();
-		exit(EXIT_FAILURE);
-	}
 
-	/* Read in the number of processes wanted */
-	sscanf(argv[1] + 1, "%d", &process_num); /* sneaky pointer math to remove - */
-	if (process_num == 0) {
-		display_error();
-		exit(EXIT_FAILURE);
-	} else if (process_num > sysconf(_SC_CHILD_MAX)) { /* sanity check */
-		fprintf(stderr, "Sorry, %d is too large for this system\n",
-		                process_num);
-		exit(EXIT_FAILURE);
-	}
-	return process_num;
 }
 
-int parseToSort(FILE *input, FILE *output)
+void close_file(int processes, FILE **streams)
 {
-	char buffer[200];
-	int read;
 
-	read = (fscanf(input, "%*[^A-Za-z']"), fscanf(input, "%198[A-Za-z']",
-	                buffer));
+	int i;
+	for (i = 0; i < processes; i++) {
+		fclose(streams[i]);
+	}
+	free(streams);
+}
 
-	if (read != EOF) {
-		strcat(buffer, "\n");
-		fputs(to_lower(buffer), output);
-		return read;
+int get_arguments(int argc, char *argv[])
+{
+
+	int process_number;
+
+	if (argc > 2 || argc < 2) {
+		fprintf(stderr, "Usage: uniqify -[number > 0]\n");
+		exit(EXIT_FAILURE);
 	} else {
-		return EOF;
+		sscanf(argv[1] + 1, "%d", &process_number);
+	}
+	return process_number;
+}
+
+void create_pipe(int processes, int **toSort, int **fromSort)
+{
+
+	int i;
+	for (i = 0; i < processes; i++) {
+		if (pipe(toSort[i]) == -1) {
+			perror("toSort pipe");
+			exit(EXIT_FAILURE);
+		}
+		if (pipe(fromSort[i]) == -1) {
+			perror("fromSort pipe");
+			exit(EXIT_FAILURE);
+		}
 	}
 }
 
-int readFromSort(FILE *to, FILE *from)
+void sorter(int processes, int **toSort, int **fromSort)
 {
 
-	char buffer[200];
-	if (fgets(buffer, 200, from) == NULL ) {
-		return EOF;
-	} else {
-		fputs(buffer, to);
+}
+
+void parser(int processes, int **toSort, int **fromSort)
+{
+
+	FILE **output;
+	char word[200];
+	int i;
+
+	output = (FILE**) calloc(processes, sizeof(FILE*));
+
+	if (output == NULL ) {
+		perror("file stream allocation");
+		exit(EXIT_FAILURE);
 	}
 
-	return 0;
+	for (i = 0; i < processes; i++) {
+		close(toSort[i][0]);
+		close(fromSort[i][0]);
+		close(fromSort[i][1]);
+		output[i] = fdopen(toSort[i][1], "w");
+	}
+	i = 0;
+	while (get_word(word) > 0) {
+		fputs(word, output[i]);
+		if (i == processes - 1) {
+			i = 0;
+		} else {
+			i++;
+		}
+	}
+	close_file(processes, output);
+}
+
+int get_word(char *word)
+{
+
+	int length = 0;
+	if (fscanf(stdin, "%198[A-Za-z']%*[^A-Za-z']", word) > 0) {
+		strcat(word, "\n");
+		to_lower(word);
+		length = strlen(word);
+	}
+	return length;
+}
+
+void read_pipe(int position, std::vector<std::string> &cache, FILE **BACK, int *timeout) {
+
 }
 
 /* http://stackoverflow.com/questions/6857445/lowercase-urls-in-varnish-inline-c */
@@ -339,9 +159,15 @@ static char *to_lower(char *str)
 	return str;
 }
 
-void display_error(void)
+void signalHandler(int signum, siginfo_t *info, void *ptr)
 {
-	char error[] =
-	                "Usage: uniqify -[number_of_processes (greater than zero)]";
-	fprintf(stderr, "\n %s\n\n\n", error);
+	int status, i;
+	//printf("\nSignal: %d\n", signum);
+	//kill(pids[0], signum);
+	//wait(&status);
+	for (i = 0; i < processes; i++) {
+		kill(pids[i], SIGKILL);
+		wait(&status);
+	}
+	exit(EXIT_FAILURE);
 }
--- homework3/uniqold1.c
+++ homework3/uniqold1.c
@@ -1,103 +1,41 @@
 #define _POSIX_C_SOURCE 199309
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
-#include <ctype.h>
-#include <string.h>
-#include <signal.h>
+#include <malloc.h>
 #include <sys/types.h>
 #include <wait.h>
-#include <malloc.h>
+#include <signal.h>
+#include <string.h>
+#include <ctype.h>
 
+pid_t *pids; /* These need to be here global for use with the signal handler */
+int process_num;
 struct sigaction act;
-//int *pid, num_proc; /* This is a global variable for usage with the signal handler */
-pid_t *pids;
-long num_proc;
 
-int parser(FILE *input, FILE *output);
-static char *to_lower(char *str);
 void signalHandler(int signum, siginfo_t *info, void *ptr);
+int input_validation(int argc, char *argv[]);
+int parseToSort(FILE *input, FILE *output);
+int readFromSort(FILE *to, FILE *from);
+static char *to_lower(char *str);
 void display_error(void);
-
 
 int main(int argc, char *argv[])
 {
+	/* Variables */
+	int i, j;
+	char mem_err[] = "Memory allocation failed";
+	char stream_err[] = "Stream allocation failed";
 
-	int i; /* The number of processes to run */
-	int **pipefdin; /* array of file descriptors for input from sorter */
-	int **pipefdout; /* array of file descriptors for output to sorter */
-	//int pipefdout[2], pipefdin[2];
-	//FILE *output, *input;
-	FILE **output;
-	FILE **input;
-	char mem_err[] = "Memory allocation failure";
-	char stream_err[] = "Stream allocation failure";
+	int **pipesortin, **pipesortout; /* for piping/plumbing to sort */
+	FILE **input, **output; /* for giving/getting data to/from sort */
 
-	/* check if correct number of arguments passed */
-	if (argc < 2) {
-		display_error();
-		exit(EXIT_FAILURE);
-	}
+	int pipeToSuppress[2], pipeFromSuppress[2]; /* Pipes to/from suppressor */
+	FILE *toSuppress, *fromSuppress; /* Stream to/from suppressor */
 
-	/* read in number of processes desired */
-	sscanf(argv[1]+1, "%ld", &num_proc); /* sneaky hack to remove - */
-	if (num_proc == 0) {
-		display_error();
-		exit(EXIT_FAILURE);
-	}
-
-	/* Check to see if number of desired processes is greater than the system
-	 * allows.
-	 */
-	if(num_proc > sysconf(_SC_CHILD_MAX)){
-		fprintf(stderr, "Sorry, %ld is too large for this system\n", num_proc);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Allocating array to hold pipe file descriptors */
-	pipefdin = (int**)calloc(num_proc, sizeof(int*));
-	if(pipefdin == NULL){
-		fprintf(stderr, "%s\n", mem_err);
-		exit(EXIT_FAILURE);
-	}
-
-	pipefdout = (int**) calloc(num_proc, sizeof(int*));
-	if (pipefdout == NULL ) {
-		fprintf(stderr, "%s\n", mem_err);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Allocate stream */
-	input = (FILE**)calloc(num_proc, sizeof(FILE*));
-	if(input == NULL){
-		fprintf(stderr, "%s\n", stream_err);
-		exit(EXIT_FAILURE);
-	}
-	output = (FILE**) calloc(num_proc, sizeof(FILE*));
-	if (output == NULL ) {
-		fprintf(stderr, "%s\n", stream_err);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Allocate space for pipe file descriptors to store */
-	for(i = 0; i < num_proc; i++){
-		pipefdout[i] = (int*)calloc(2, sizeof(int));
-		if(pipefdout[i] == NULL){
-			fprintf(stderr, "%s\n", mem_err);
-			exit(EXIT_FAILURE);
-		}
-		pipefdin[i] = (int*)calloc(2, sizeof(int));
-		if(pipefdin[i] == NULL){
-			fprintf(stderr, "%s\n", mem_err);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	/* Allocate array of pids */
-	pids = (pid_t*)calloc(num_proc, sizeof(pid_t));
-
+	pid_t suppressor;
 
 	/* Setting up signal handling */
 	memset(&act, 0, sizeof(act));
@@ -107,95 +45,220 @@ int main(int argc, char *argv[])
 	sigaction(SIGQUIT, &act, NULL );
 	sigaction(SIGHUP, &act, NULL );
 
-	/* Begin piping and forking processes */
-	for (i = 0; i < num_proc; i++) {
-		if (pipe(pipefdin[i]) == -1) {
-			perror("pipes");
+
+	process_num = input_validation(argc, argv);
+
+	/* allocate pipes holding pipes */
+	pipesortin = (int**) calloc(process_num, sizeof(int*));
+	pipesortout = (int**) calloc(process_num, sizeof(int*));
+	if (pipesortin == NULL || pipesortout == NULL ) {
+		fprintf(stderr, "%s\n", mem_err);
+		exit(EXIT_FAILURE);
+	}
+	/* Allocate pipes */
+	for (i = 0; i < process_num; i++) {
+		pipesortin[i] = (int*) calloc(2, sizeof(int));
+		pipesortout[i] = (int*) calloc(2, sizeof(int));
+		if (pipesortin[i] == NULL || pipesortout[i] == NULL ) {
+			fprintf(stderr, "%s\n", mem_err);
+			free(pipesortout);
+			free(pipesortin);
 			exit(EXIT_FAILURE);
 		}
-		if (pipe(pipefdout[i]) == -1) {
-			perror("pipes");
+	}
+
+	/* allocate file streams */
+	input = (FILE**) calloc(process_num, sizeof(FILE*));
+	output = (FILE**) calloc(process_num, sizeof(FILE*));
+	if (input == NULL || output == NULL ) {
+		fprintf(stderr, "%s\n", stream_err);
+		free(input);
+		free(output);
+		free(pipesortin);
+		free(pipesortout);
+		exit(EXIT_FAILURE);
+	}
+
+	/* Allocate holder for PIDs */
+	pids = (pid_t*) calloc(process_num, sizeof(pid_t));
+
+	/* pipe and plumb */
+	for (i = 0; i < process_num; i++) {
+		if (pipe(pipesortin[i]) == -1 || pipe(pipesortout[i]) == -1) {
+			perror("sort pipes");
 			exit(EXIT_FAILURE);
 		}
+
+		/* Create processes (fork) */
 		switch (pids[i] = fork()) {
+
 		case -1:
+			/* error */
 			perror("process not created");
-			free(pipefdin);
-			free(pipefdout);
-			free(input);
-			free(output);
 			exit(EXIT_FAILURE);
-			break;
 
 		case 0:
-			/* child */
-			close(pipefdout[i][1]); /* close sort write fd */
-			close(pipefdin[i][0]); /* close sort out's read fd */
-			dup2(pipefdout[i][0], STDIN_FILENO); /* set sort in as parent out */
-			dup2(pipefdin[i][1], STDOUT_FILENO);/* set sort out as output */
-			close(pipefdout[i][0]); /* close it's remapped fd */
-			close(pipefdin[i][1]); /* close it's remapped fd */
-			execlp("sort", "sort", (char *) NULL );
+			/* Child stuff and plumbing */
+			/* close sort write fd */
+			if (close(pipesortout[i][1]) == -1) {
+				perror("close sort in write");
+				_exit(EXIT_FAILURE);
+			}
+			/* close sort out's read fd */
+			if (close(pipesortin[i][0]) == -1) {
+				perror("close sort out read");
+				_exit(EXIT_FAILURE);
+			}
+			/* set sort in as parent out */
+			if (dup2(pipesortout[i][0], STDIN_FILENO) == -1) {
+				perror("mapping sort stdin");
+				_exit(EXIT_FAILURE);
+			}
+			/* set sort out as output */
+			if (dup2(pipesortin[i][1], STDOUT_FILENO) == -1) {
+				perror("mapping sort stdout");
+				_exit(EXIT_FAILURE);
+			}
+			/* close it's remapped fd */
+			if (close(pipesortout[i][0]) == -1) {
+				perror(
+				                "closing sort unused pipe in from remap");
+				_exit(EXIT_FAILURE);
+			}
+			/* close it's remapped fd */
+			if (close(pipesortin[i][1]) == -1) {
+				perror(
+				                "closing sort unused pipe out from remap");
+				_exit(EXIT_FAILURE);
+			}
+			if(execlp("sort", "sort", (char *) NULL ) == -1){
+				perror("sort exec failed");
+				_exit(EXIT_FAILURE);
+			}
 			break;
 
 		default:
-			/* parent */
-			output[i] = fdopen(pipefdout[i][1], "w");
-			input[i] = fdopen(pipefdin[i][0], "r");
-
-			close(pipefdout[i][0]);
-			close(pipefdin[i][1]);
+			/* Parent stuff and plumbing */
+			output[i] = fdopen(pipesortout[i][1], "w");
+			input[i] = fdopen(pipesortin[i][0], "r");
+			if (output[i] == NULL || input[i] == NULL ) {
+				perror("stream to sort");
+				exit(EXIT_FAILURE);
+			}
+			if (close(pipesortout[i][0]) == -1) {
+				perror("closing sort unneeded fd");
+				exit(EXIT_FAILURE);
+			}
+			if (close(pipesortin[i][1]) == -1) {
+				perror("closing unneeded fd");
+				exit(EXIT_FAILURE);
+			}
 			break;
 		}
 	}
 
-
-
-	/* parse */
+	/* Parse input from stdin to sort */
 	i = 0;
-	while (parser(stdin, output[i]) > 0) {
-		if (i == num_proc - 1) {
+	while (parseToSort(stdin, output[i]) != EOF) {
+		if (i + 1 == process_num) {
 			i = 0;
 		} else {
 			i++;
 		}
 	}
-	for(i = 0; i< num_proc; i++){
+	fclose(stdin);
+	for (i = 0; i < process_num; i++) {
 		fclose(output[i]);
 	}
-	//while (parser(stdin, output[0]) > 0);
-	//fclose(output[0]);
 
-	/* TEMP: read from all the pipes to make sure
-	 * 	exec/forking/piping process works */
+
+	/* Pipe the suppressor process */
+	if (pipe(pipeToSuppress) == -1 || pipe(pipeFromSuppress) == -1) {
+		perror("piping suppressor");
+		exit(EXIT_FAILURE);
+	}
+
+	/* create the suppressor process */
+	switch (suppressor = fork()) {
+
+	case -1: /* Error creating child */
+		perror("suppressor process error");
+		exit(EXIT_FAILURE);
+
+	case 0: /* Child process setup */
+		/* Close suppress in write fd */
+		if (close(pipeToSuppress[1]) == -1) {
+			perror("closing suppress in write fd");
+			_exit(EXIT_FAILURE);
+		}
+		/*Close suppress out read fd */
+		if (close(pipeFromSuppress[0]) == -1) {
+			perror("closing suppress out read fd");
+			_exit(EXIT_FAILURE);
+		}
+		/* Set suppressor in as parent out */
+		if (dup2(pipeToSuppress[0], STDIN_FILENO) == -1) {
+			perror("mapping suppressor in as parent out");
+			_exit(EXIT_FAILURE);
+		}
+		/* Set suppressor out as parent in */
+		if (dup2(pipeFromSuppress[1], STDOUT_FILENO) == -1) {
+			perror("mapping suppressor out as parent in");
+			_exit(EXIT_FAILURE);
+		}
+		/* Cleanup the unused pipe FDs */
+		if (close(pipeToSuppress[0]) == -1
+		                || close(pipeFromSuppress[1]) == -1) {
+			perror("closing old pipe FDs");
+			_exit(EXIT_FAILURE);
+		}
+		/* exec suppressor executable */
+		if(execl("./suppressor", "suppressor", NULL) == -1){
+			perror("failed to exec suppressor");
+			_exit(EXIT_FAILURE);
+		}
+		break;
+
+	default: /* plumb for the parent side */
+		/* Open file streams to/from suppressor */
+		toSuppress = fdopen(pipeToSuppress[1], "w");
+		fromSuppress = fdopen(pipeFromSuppress[0], "r");
+		if (toSuppress == NULL || fromSuppress == NULL ) {
+			perror("stream for suppressor");
+			exit(EXIT_FAILURE);
+		}
+		/* Close unneeded FDs */
+		if (close(pipeToSuppress[1]) == -1) {
+			perror("closing suppressor to pipe");
+			exit(EXIT_FAILURE);
+		}
+		if (close(pipeFromSuppress[0]) == -1) {
+			perror("closing suppressor from pipe");
+			exit(EXIT_FAILURE);
+		}
+		break;
+	}
+
+	/* Read from sorters round-robin, and suppress (for now, print to stdout) */
+
 	i = 0;
-	while(parser(input[i], stdout) > 0){
-		if(i == num_proc - 1){
+	while (readFromSort(stdout, input[i]) != EOF) {
+		if (i + 1 == process_num) {
 			i = 0;
 		} else {
 			i++;
 		}
 	}
-	//while (parser(input[0], stdout) > 0);
-	//fclose(input[0]);
-	for(i = 0; i < num_proc; i++){
-		fputs(EOF, input[i]);
+
+	for (i = 0; i < process_num; i++) {
 		fclose(input[i]);
 	}
 
-	/* cleanup the arrays */
-	free(pipefdin);
-	free(pipefdout);
 	free(input);
 	free(output);
-
+	free(pipesortin);
+	free(pipesortout);
 	exit(EXIT_SUCCESS);
-}
-
-void display_error(void)
-{
-	char error[] = "Usage: uniqify -[number_of_processes (greater than zero)]";
-	fprintf(stderr, "\n %s\n\n\n", error);
 }
 
 void signalHandler(int signum, siginfo_t *info, void *ptr)
@@ -204,29 +267,63 @@ void signalHandler(int signum, siginfo_t *info, void *ptr)
 	//printf("\nSignal: %d\n", signum);
 	//kill(pids[0], signum);
 	//wait(&status);
-	for(i = 0; i < num_proc; i++){
-		kill(pids[i], signum);
+	for (i = 0; i < process_num; i++) {
+		kill(pids[i], SIGKILL);
 		wait(&status);
 	}
 	exit(EXIT_FAILURE);
 }
 
-int parser(FILE *input, FILE *output)
+int input_validation(int argc, char *argv[])
+{
+	int process_num;
+	/* Check that only number of processes is passed as argument */
+	if (argc < 2 || argc > 2) {
+		display_error();
+		exit(EXIT_FAILURE);
+	}
+
+	/* Read in the number of processes wanted */
+	sscanf(argv[1] + 1, "%d", &process_num); /* sneaky pointer math to remove - */
+	if (process_num == 0) {
+		display_error();
+		exit(EXIT_FAILURE);
+	} else if (process_num > sysconf(_SC_CHILD_MAX)) { /* sanity check */
+		fprintf(stderr, "Sorry, %d is too large for this system\n",
+		                process_num);
+		exit(EXIT_FAILURE);
+	}
+	return process_num;
+}
+
+int parseToSort(FILE *input, FILE *output)
+{
+	char buffer[200];
+	int read;
+
+	read = (fscanf(input, "%*[^A-Za-z']"), fscanf(input, "%198[A-Za-z']",
+	                buffer));
+
+	if (read != EOF) {
+		strcat(buffer, "\n");
+		fputs(to_lower(buffer), output);
+		return read;
+	} else {
+		return EOF;
+	}
+}
+
+int readFromSort(FILE *to, FILE *from)
 {
 
 	char buffer[200];
-
-	int read;
-
-	read = (fscanf(input, "%*[^A-Za-z]"), fscanf(input, "%198[a-zA-Z]",
-	                buffer));
-
-	if (read > 0) {
-		strcat(buffer, "\n");
-		//fprintf(output, "%s", to_lower(buffer));
-		fputs(to_lower(buffer), output);
+	if (fgets(buffer, 200, from) == NULL ) {
+		return EOF;
+	} else {
+		fputs(buffer, to);
 	}
-	return read;
+
+	return 0;
 }
 
 /* http://stackoverflow.com/questions/6857445/lowercase-urls-in-varnish-inline-c */
@@ -240,4 +337,11 @@ static char *to_lower(char *str)
 		s++;
 	}
 	return str;
+}
+
+void display_error(void)
+{
+	char error[] =
+	                "Usage: uniqify -[number_of_processes (greater than zero)]";
+	fprintf(stderr, "\n %s\n\n\n", error);
 }
