54a2a4e94b74475afb3c0e6dba01e7a642ed5702
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-24 15:06:27 -0800

    added initial code files for the project, minus the planning files

---
+++ homework4/primes.c
@@ -0,0 +1,17 @@
+//#define _POSIX_C_SOURCE 199309
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <math.h>
+
+
+int main(int argc, char *argv[])
+{
+
+
+
+	exit(EXIT_SUCCESS);
+}
