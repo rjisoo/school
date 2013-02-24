512d11e7ce4f14b74935eb0904e5910ed1fb590e
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-24 15:11:55 -0800

    fixed initial bitmask array problem, had to malloc instead of declare

--- homework4/primes.c
+++ homework4/primes.c
@@ -6,12 +6,21 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <math.h>
+#include <malloc.h>
 
 
 int main(int argc, char *argv[])
 {
 
+	uint8_t *bitmask;
 
+	bitmask = (uint8_t*)calloc(UINT32_MAX/8, sizeof(uint8_t));
+	if(bitmask == NULL){
+		perror("memory allocation");
+		exit(EXIT_FAILURE);
+	}
+
+	sleep(50);
 
 	exit(EXIT_SUCCESS);
 }
