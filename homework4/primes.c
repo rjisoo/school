02280b563b7ea69f2aa093d66e2dbe5a87262634
Geoffrey Corey <coreyg@onid.orst.edu>
2013-02-27 10:11:48 -0800

    something broke and had to rewrite primes with bitmask. got it working again, not to figure out segmenting and then threading

--- homework4/primes.c
+++ homework4/primes.c
@@ -8,69 +8,114 @@
 #include <math.h>
 #include <malloc.h>
 
-long sieve(uint8_t *sieve, long limit);
-uint8_t *initialize(long limit);
 
-#define MAX 4294967296
+#define MAX 4294967295
 #define COMPOSITE 1
+#define PRIME 1
 
+
+uint8_t *sieve_init(long limit);
+long sieving(uint8_t *list, long limit);
+void mark_composite(uint8_t *list, long index, long limit, long *count);
 
 int main(int argc, char *argv[])
 {
 
 	uint8_t *list;
-	long limit = MAX;
+	long limit;
 	long result;
 
-	list = initialize(limit);
+	limit = MAX;
 
-	result = sieve(list, limit);
+#ifdef DEBUG
+	fprintf(stdout, "limit is: %ld\n", limit);
+#endif
 
-	printf("result: %ld\n", result);
+	list = sieve_init(limit);
+
+#ifdef DEBUG
+	if(list != NULL){
+		fprintf(stdout, "Sieve initialized properly.\n");
+	}
+#endif
+
+	result = sieving(list, limit);
+
+	fprintf(stdout, "Number of primes found: %ld.\n", result);
 
 	//sleep(5);
-
 	free(list);
 	exit(EXIT_SUCCESS);
 }
 
-long sieve(uint8_t *sieve, long limit)
+uint8_t *sieve_init(long limit)
 {
-	long i, j, count;
+	uint8_t *temp;
+#ifdef DEBUG
+	fprintf(stdout, "Initializing the list.\n");
+#endif
+	temp = (uint8_t*)calloc((limit+1)/8, sizeof(uint8_t));
+	if(temp == NULL){
+		fprintf(stderr, "Memory allocation failure.\n");
+		exit(EXIT_FAILURE);
+	} else {
+		return temp;
+	}
+
+}
+
+long sieving(uint8_t *list, long limit)
+{
+
+	long i, count;
 	int rootlimit;
 
 	rootlimit = sqrt(limit);
 	count = limit -1;
 
-	//sieve[0] = sieve[1] = 0; // mark special, but we don't really care about these.
-	//sieve[0>>3] |= (!COMPOSITE<<(0&(8-1)));
-	//sieve[1>>3] |= (!COMPOSITE<<(1&(8-1)));
+#ifdef DEBUG
+	fprintf(stdout, "sqrt of %ld: %d.\n", limit, rootlimit);
+	fprintf(stdout, "count starts: %ld.\n", count);
+#endif
+	list[0] = list[1] = COMPOSITE; //these are really just special values
 
+	/* beginning sieve */
 	for(i = 2; i <= rootlimit; i++){
-
-		if(!(sieve[i>>3] & (COMPOSITE<<(i&(8-1))))){ //if sieve[i] == 0
-			count++;
-			for(j = i*i; j <= MAX; j += i){
-				if(!(sieve[j>>3] & (COMPOSITE<<(j&(8-1))))){ //if sieve[j] == 0
-					sieve[j>>3] |= (COMPOSITE<<(j&(8-1))); //sieve[i] = 1
-					count--;
-				}
-			}
+		/*if(list[i] == PRIME)*/
+		if(!(list[i>>3]&(COMPOSITE<<(i&(8-1))))){
+			mark_composite(list, i, limit, &count);
+			/* we found a prime, mark its multiples */
+			//for(j = i*i; j <= limit; j += i){
+				/*if(list[j] == 0)*/
+				//if(!(list[j>>3]&(COMPOSITE<<(j&(8-1))))){
+					/* Found a multiple, mark it composite */
+					/* list[j] = 1 */
+				//	list[j>>3] |= (COMPOSITE<<(j&(8-1)));
+				//	count--;
+				//}
+			//}
 		}
 	}
+#ifdef DEBUG
+	fprintf(stdout, "counted primes: %ld.\n", count);
+#endif
+
 
 	return count;
 }
 
-uint8_t *initialize(long limit)
+void mark_composite(uint8_t *list, long index, long limit, long *count)
 {
-	uint8_t *sieve;
-	sieve = (uint8_t*)calloc((limit+1)/8, sizeof(uint8_t));
-	if(sieve == NULL){
-		fprintf(stderr, "Memory application failure.\n");
-		exit(EXIT_FAILURE);
-	} else {
-		return sieve;
-	}
 
+	long j;
+
+	for(j = index*index; j <= limit; j += index){
+		/*if(list[j] == 0)*/
+		if(!(list[j>>3]&(COMPOSITE<<(j&(8-1))))){
+			/* Found a multiple, mark it composite */
+			/* list[j] = 1 */
+			list[j>>3] |= (COMPOSITE<<(j&(8-1)));
+			--(*count);
+		}
+	}
 }
