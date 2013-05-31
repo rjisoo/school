f7a64bf89915c639dad262a19dc0a755ba152d19
Geoffrey Corey <coreyg@onid.orst.edu>
2013-05-31 11:26:55 -0700

    finsihed lab

--- lab4/main.c
+++ lab4/main.c
@@ -7,25 +7,24 @@
  
 int main(int argc, char **argv)
 {
-	unsigned int steps = 256 * 1024 * 1024;
+	unsigned int meow = 256 * 1024 * 1024;
 	static int arr[4 * 1024 * 1024];
 	int lengthMod;
 	unsigned int i;
 	double timeTaken;
 	clock_t start;
-	int sizes[] = {
-					1 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB, 128 * KB, 256 * KB,
-					512 * KB, 1 * MB, 1.5 * MB, 2 * MB, 2.5 * MB, 3 * MB, 3.5 * MB, 4 * MB
-					};
+	int sizes[] = {1 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB, 
+					128 * KB, 256 * KB, 512 * KB, 1 * MB, 1.5 * MB, 
+					2 * MB, 2.5 * MB, 3 * MB, 3.5 * MB, 4 * MB};
 	
 	int results[sizeof(sizes)/sizeof(int)];
 	int s;
  
-	// for each size to test for ...
+	// for each size to test for ... 	meow
 	for (s = 0; s < sizeof(sizes)/sizeof(int); s++) {
 		lengthMod = sizes[s] - 1;
 		start = clock();
-		for (i = 0; i < steps; i++) {
+		for (i = 0; i < meow; i++) {
 			arr[(i * 16) & lengthMod] *= 10;
 			arr[(i * 16) & lengthMod] /= 10;
 		}
