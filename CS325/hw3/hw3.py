82714b450d9d39a91546e0920e3f5515747fedb5
Geoffrey Corey <coreyg@onid.orst.ed>
2013-11-07 08:49:24 -0800

    finished project

--- CS325/hw3/hw3.py
+++ CS325/hw3/hw3.py
@@ -9,24 +9,22 @@ import time
 def changegreedy(coins, amount):
 	denoms = sorted(coins) #sanity check
 	denoms.reverse() 	#sanity check
-	coins = 0
+	result = []
 	start = time.time()
 	while (amount > 0):
 		if (amount >= denoms[0]):
 			num = amount / denoms[0]
 			amount -= (num * denoms[0])
-			coins += num
+			result.append([denoms[0], num])
 		denoms = denoms[1:]
-	end = time.time()
-	return coins
-	#return end - start
-
+	print result
+	#return result
+	
 
 def changedp (coins, change):
 	min_coins = [None for x in range(change + 1)]
 	min_coins[0] = []
 
-	start = time.time()
 	for i in range(1, change + 1):
 		for coin in coins:
 			if coin >  i: continue
@@ -34,22 +32,16 @@ def changedp (coins, change):
 				if min_coins[i - coin] != None:
 					min_coins[i] = min_coins[i - coin][:]
 					min_coins[i].append(coin)
-	end = time.time()
-
-	return len(min_coins[-1])
-	#return end - start
+	print '%d coins: %s' % (len(min_coins[-1]), min_coins[-1])
 
 
 if __name__ == "__main__":
-	coins = [1, 5, 10, 25, 50]
+	# Set the coins here manually
+	coins = [1, 10, 25, 50]
 
-	for x in range(2000, 2301):
-		print ("%d 	%d 	%d" % (x, changegreedy(coins, x), changedp(coins, x)))
-		#greedy = []
-		#dp = []
-		
-		#for i in range(0, 10):
-		#	greedy.append(changegreedy(coins, x))
-		#	dp.append(changedp(coins, x))
+	# Set the change desired here manually
+	change = 40
 
-		#print ("%d 	%f 	%f" % (x, reduce(lambda x, y: x + y, greedy) / len(greedy), reduce(lambda x, y: x + y, dp) / len(dp)))
+	print coins
+	changegreedy(coins, change)
+	changedp(coins, change)
