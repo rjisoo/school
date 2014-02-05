91013c07199d622a8fe134ff7b9607a7190da71d
Geoffrey Corey <coreyg@onid.orst.ed>
2014-02-04 20:24:23 -0800

    finsihed number 2

--- CS381/hw2/Hw2.hs
+++ CS381/hw2/Hw2.hs
@@ -10,6 +10,8 @@ data Step = Go Dir Int deriving Show
 
 type Move = [Step]
 
+type Moves = [Dir]
+
 type Pos = (Int, Int)
 type Dist = Int
 type Trip = (Pos, Dist)
@@ -23,4 +25,51 @@ semInt :: Step -> Int
 semInt (Go _ r) = r
 
 -- b)
---semTrip :: Move -> Trip
+--semTrip :: Move -> Pos
+--semTrip [] = (0,0)
+--semTrip (x:xs) = 
+
+semTest :: Step -> Dir
+semTest (Go r _) = r
+
+semDir :: Dir -> Pos
+semDir Lft = (-1,0)
+semDir Rgt = (1,0)
+semDir Up = (0,1)
+semDir Dwn = (0,-1)
+
+-- 2.
+
+type Prog = [Cmd]
+
+data Cmd = LD Int
+         | ADD
+         | MULT
+         | DUP
+
+type Stack = [Int]
+
+type D = Maybe Stack -> Maybe Stack
+
+sem :: Prog -> D
+sem [] a = a
+sem (o:os) b = sem os (semCmd o b)
+
+semCmd :: Cmd -> D
+semCmd (LD e) xs = case xs of
+                        Just xs -> if e < 0
+                             then Nothing
+                             else Just (xs ++ [e])
+semCmd ADD xs = case xs of 
+                     Just xs -> if length xs < 2
+                          then Nothing
+                          else Just (init(init xs) ++ [last xs + last(init xs)])
+semCmd MULT xs = case xs of
+                      Just xs -> if length xs < 2
+                           then Nothing
+                           else Just (init(init xs) ++ [last xs * last(init xs)])
+semCmd DUP xs = case xs of
+                     Just xs -> if length xs < 1
+                          then Nothing
+                          else Just (xs ++ [(last xs)]) 
+
