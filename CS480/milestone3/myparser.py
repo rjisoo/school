55785cb0a3a1888a8eebfa1a79b64f27560ec012
Geoffrey Corey <coreyg@onid.orst.ed>
2014-02-26 22:30:30 -0800

    got verbose parsing of [[]] working

--- CS480/milestone3/myparser.py
+++ CS480/milestone3/myparser.py
@@ -12,14 +12,74 @@ def parser(stream):
   global index
   tokens = list(myreglexer.tokenize(stream))
   T(tokens)
+  print index
 
 def T(tokens): # T -> [S]
   global index
+
+  print tokens[index]
+
+  if tokens[index][0] == "LBRACE": # [
+    
+    # S
+    nextToken()
+    print tokens[index]
+    S(tokens)
+
+    # ]
+    nextToken()
+    print tokens[index]
+    if not tokens[index][0] == "RBRACE":
+      error()
+    else:
+      # Ready to parse statement
+      nextToken()
+      #print tokens[index]
+  else:
+    error()
 
 def S(tokens): # S -> expr S_ | []S_ | [S]S_
   global index
   global ahead1
 
+  if (
+      tokens[ahead1][0] == "BINOP" or
+      tokens[ahead1][0] == "UNOP" or
+      tokens[ahead1][0] == "NAME" or
+      tokens[ahead1][0] == "ASSIGN" or
+      tokens[ahead1][0] == "STATEMENT"
+      ):
+    # S -> expr S_
+    expr(tokens)
+    nextToken()
+    print tokens[index]
+    S_(tokens)
+
+  elif tokens[index][0] == "LBRACE" and tokens[ahead1][0] == "RBRACE": # S -> []S_
+    nextToken()
+    print tokens[index]
+    if not tokens[ahead1][0] == "RBRACE":
+      nextToken()
+      print tokens[index]
+      S_(tokens)
+
+  elif tokens[index][0] == "LBRACE":
+    # S -> [S]S_
+    # [
+    nextToken()
+    print tokens[index]
+    S(tokens) # S
+    nextToken()
+    print tokens[index]
+    if not tokens[index] == "RBRACE":
+      error()
+    if not tokens[ahead1][0] == "RBRACE":
+      nextToken()
+      print tokens[index]
+      S_(tokens)
+
+  else:
+    return
 
 def S_(tokens): # S_ -> SS_ | empty
   global index
