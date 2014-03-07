f4a12dd96b363443650216646e037ae1df2dd354
Geoffrey Corey <coreyg@onid.orst.ed>
2014-03-06 18:10:51 -0800

    restructuring the driver

--- CS480/milestone4/driver.py
+++ CS480/milestone4/driver.py
@@ -7,9 +7,6 @@ import myparser
 contents = []
 
 def main(argv):
-  lexemes = []
-  x = 1
-
   try:
     opts, args = getopt.getopt(argv, "h", ["help"])
   except getopt.GetoptError:
@@ -25,19 +22,12 @@ def main(argv):
   for arg in args:
     try:
       with open(arg, 'r') as f:
-        #contents.append(f.read())
         readin = f.read()
         myparser.parser(readin)
 
     except IOError:
       print "File %s not found!" % arg
       sys.exit(1)
-
-  #tokens = myreglexer.tokenize("".join(contents))
-  #with open(argument, 'r') as f:
-  #  contents = f.read()
-
-  #myparser.parser(''.join(contents))
 
 def no_args():
   for line in sys.stdin:
