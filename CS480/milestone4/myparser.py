0b2233854039ad6dda537a1ade6a0eefbdd08422
Geoffrey Corey <coreyg@onid.orst.ed>
2014-03-08 14:04:45 -0800

    Fixed a bug with how S goes to expr

--- CS480/milestone4/myparser.py
+++ CS480/milestone4/myparser.py
@@ -2,168 +2,175 @@
 import collections
 import myreglexer
 import sys
+from tree import *
 
 index = 0
 ahead1 = 1
 
 stack = []
 
-
-
 def parser(stream):
   global stack, index, ahead1
   tokens = list(myreglexer.tokenize(stream))
   index = 0
   ahead1 = 1
-  T(tokens)
-  #print stack
+  root = Node()
+  root.addChild(T(tokens))
+
+  root.traverse()
 
 def T(tokens): # T -> [S]
   global index, stack
 
-  if tokens[index][0] == "LBRACE":
-    
-    stack.append(tokens[index][1])
-    nextToken()
-    
-    S(tokens)
+  temp = Node()
 
-    stack.append(tokens[index][1])
+  if tokens[index][0] == "LBRACE":
+    temp.addChild(Node(tokens[index][1]))
+    
+    nextToken()
+    temp.addChild(S(tokens))
+    
     nextToken()
     if not tokens[index][0] == "RBRACE":
       error(tokens[index], ']')
 
-    stack.append(tokens[index][1])
-    nextToken()
+    temp.addChild(Node(tokens[index][1]))
 
-    return not None
+    return temp
   else:
     error(tokens[index], '[')
 
 def S(tokens): # S -> expr S_ | []S_ | [S]S_
   global index, ahead1, stack
 
-  if (tokens[ahead1][0] == "BINOP" or
-      tokens[ahead1][0] == "UNOP" or
-      tokens[ahead1][0] == "MINUS" or
-      tokens[ahead1][0] == "NAME" or
-      tokens[ahead1][0] == "BOOL" or
-      tokens[ahead1][0] == "ASSIGN" or
-      tokens[ahead1][0] == "STATEMENT" or
-      tokens[ahead1][0] == "REAL" or
-      tokens[ahead1][0] == "INTEGER" or
-      tokens[ahead1][0] == "STRING"):
+  temp = Node()
+
+  if (tokens[ahead1][0] == "BINOP" or tokens[ahead1][0] == "UNOP" or
+      tokens[ahead1][0] == "MINUS" or tokens[ahead1][0] == "ASSIGN" or
+      tokens[ahead1][0] == "STATEMENT" or tokens[index][0] == "NAME" or 
+      tokens[index][0] == "BOOL" or tokens[index][0] == "REAL" or
+      tokens[index][0] == "INTEGER" or tokens[index][0] == "STRING"):
     # S -> expr S_
-    expr(tokens)
+    temp.addChild(expr(tokens))
+
     if not tokens[ahead1][0] == "RBRACE":
-      stack.append(tokens[index][1])
       nextToken()
-      S_(tokens)
+      temp.addChild(S_(tokens))
 
   elif tokens[index][0] == "LBRACE" and tokens[ahead1][0] == "RBRACE":
     # S -> []S_
-    stack.append(tokens[index][1])
+    temp.addChild(Node(tokens[index][1])) # [
     nextToken()
+    temp.addChild(Node(tokens[index][1])) # ]
     if not tokens[ahead1][0] == "RBRACE":
-      stack.append(tokens[index][1])
       nextToken()
-      S_(tokens)
+      temp.addChild(S_(tokens)) # S_
 
   elif tokens[index][0] == "LBRACE":
-    stack.append(tokens[index][1])
+    # S -> [S]S_
+    temp.addChild(Node(tokens[index][1])) # [
     nextToken()
-    S(tokens)
-    stack.append(tokens[index][1])
+    temp.addChild(S(tokens)) # S
     nextToken()
 
     if not tokens[index][0] == "RBRACE":
       error(tokens[index], ']')
 
+    temp.addChild(Node(tokens[index][1])) # ]
+
     if not tokens[ahead1][0] == "RBRACE":
-      stack.append(tokens[index][1])
       nextToken()
-      S_(tokens)
+      temp.addChild(S_(tokens)) # S_
 
   else:
     return None
 
-  return not None
+  return temp
 
 def S_(tokens): # S_ -> SS_ | empty
   global index, ahead1, stack
 
+  temp = Node()
+
   resultS = S(tokens)
 
   if resultS:
+    temp.addChild(resultS) # S
+    # Handle empty case
     if tokens[ahead1][0] == "RBRACE":
-      return not None
+      return temp
 
-    stack.append(tokens[index][1])
     nextToken()
 
     resultS_ = S_(tokens)
     if resultS_:
-      pass
+      temp.addChild(resultS_) # S_
 
-    return not None
+    return temp
 
   else:  
     return None
+
+  return temp
 
 
 def expr(tokens): # expr -> oper | stmts
   global index, ahead1
 
-  if tokens[ahead1][0] == "STATEMENT":
-    stmts(tokens)
+  temp = Node()
 
-  elif (tokens[ahead1][0] == "BINOP" or
-        tokens[ahead1][0] == "UNOP" or
-        tokens[ahead1][0] == "MINUS" or
-        tokens[index][0] == "NAME" or
-        tokens[index][0] == "BOOL" or
-        tokens[ahead1][0] == "ASSIGN" or
-        tokens[index][0] == "REAL" or
-        tokens[index][0] == "INTEGER" or
+  if tokens[ahead1][0] == "STATEMENT":
+    temp.addChild(stmts(tokens))
+    return temp
+
+  elif (tokens[ahead1][0] == "BINOP" or tokens[ahead1][0] == "UNOP" or
+        tokens[ahead1][0] == "MINUS" or tokens[ahead1][0] == "ASSIGN" or
+        tokens[index][0] == "NAME" or tokens[index][0] == "BOOL" or
+        tokens[index][0] == "REAL" or tokens[index][0] == "INTEGER" or
         tokens[index][0] == "STRING"):
-    oper(tokens)
+    temp.addChild(oper(tokens))
+    return temp
 
   else:
-    error(tokens[ahead1], "statement or oper")
+    error(tokens[index], "statement or oper")
   
-  return not None
+  return temp
 
 def oper(tokens): # oper -> [:= name oper] | [binops oper oper] | [unops oper] | constants | name
   global index, ahead1, stack
+
+  temp = Node()
 
   if (tokens[index][0] == "REAL" or
       tokens[index][0] == "INTEGER" or
       tokens[index][0] == "STRING" or
       tokens[index][0] == "BOOL" or
       tokens[index][0] == "NAME"):
-    return not None
+    temp.addChild(Node(tokens[index][1]))
+    return temp
 
   elif tokens[index][0] == "LBRACE":
-    stack.append(tokens[index][1])
+    temp.addChild(Node(tokens[index][1]))
     nextToken()
 
     # special case for '-' sign, because it can be binop OR unop
     if tokens[index][0] == "MINUS":
-      stack.append(tokens[index][1])
+      temp.addChild(Node(tokens[index][1]))
       nextToken()
-      oper(tokens)
+      temp.addChild(oper(tokens))
 
       if not tokens[ahead1][0] == "RBRACE":
-        stack.append(tokens[index][1])
+        #stack.append(tokens[index][1])
         nextToken()
-        oper(tokens)
+        temp.addChild(oper(tokens))
 
-      stack.append(tokens[index][1])
+      #stack.append(tokens[index][1])
       nextToken()
       if not tokens[index][0] == "RBRACE":
         error(tokens[index], ']')
 
-      return not None
+      temp.addChild(Node(tokens[index][1]))
+      return temp
 
       
 
