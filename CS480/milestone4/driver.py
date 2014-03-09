6520c48c04cd586b5ba75d0d9dafcaae987790e6
Geoffrey Corey <coreyg@onid.orst.ed>
2014-03-08 16:00:16 -0800

    testing out infix notation printing

--- CS480/milestone4/driver.py
+++ CS480/milestone4/driver.py
@@ -25,7 +25,7 @@ def main(argv):
       with open(arg, 'r') as f:
         readin = f.read()
         tree = parser(readin)
-        tree.traverse_post()
+        tree.traverse_in()
 
     except IOError:
       print "File %s not found!" % arg
