55fbbba9a3982d70b8e00702748f3361939d32a8
Geoffrey Corey <geoffcorey7@gmail.com>
2014-05-11 14:24:21 -0700

    Initial project 4 upload

---
+++ Project4/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <omp.h>
+#include <string.h>
+
+
+int main(int argc, char **argv){
+
+  exit(EXIT_SUCCESS);
+}
