7c702c9fb7349df77e5f8f32b77e50a75b564d3f
Geoffrey Corey <geoffcorey7@gmail.com>
2014-05-24 20:19:04 -0700

    Actually finished project 5

--- Project5/test.bash
+++ Project5/test.bash
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-for i in {1..13}
+#for i in {1..13}
+#do
+#  for j in {1..10}
+#  do
+#    g++ -fopenmp -O3 -g -w -march=native -I. first.cpp -o first -lrt -lpthread -lm -lOpenCL -D"LOCAL=$((2 ** $j))" -D"GLOBAL=$((2 ** $i))"
+#    optirun ./first
+#  done
+#done
+
+
+for i in 1024 2048 3072 4096 5120 6144 7168 8192
 do
-  for j in {1..10}
-  do
-    g++ -fopenmp -O3 -g -w -march=native -I. first.cpp -o first -lrt -lpthread -lm -lOpenCL -D"LOCAL=$((2 ** $j))" -D"GLOBAL=$((2 ** $i))"
-    optirun ./first
-  done
+  g++ -fopenmp -O3 -g -w -march=native -I. first.cpp -o first -lrt -lpthread -lm -lOpenCL -D"GLOBAL=$i"
+  optirun ./first
 done
