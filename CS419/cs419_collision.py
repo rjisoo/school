5972eb48789a0d05a4b242ec76587b6d611da294
Geoffrey Corey <coreyg@apache.org>
2014-11-25 08:15:26 -0800

    remove password feild for no longer existing acct

--- CS419/cs419_collision.py
+++ CS419/cs419_collision.py
@@ -8,7 +8,6 @@ import smtplib
 fromaddr = 'yourapanzy91@gmail.com'
 toaddr = 'yourapanzy91+crypto@gmail.com'
 username = 'yourapanzy91'
-password = 'Geoff5241'
 
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
